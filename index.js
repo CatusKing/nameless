38d91c0608cd81f9ed8f85d153ae71c285dbdb28
CactusKing101 <chalkking17@gmail.com>
2021-03-24 20:11:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -205,7 +205,11 @@ client.on('message', async msg => {
       .filter(user => client.users.cache.has(user.user_id))
       .first(temp)
       .forEach((user, position) => {
-        description += `\n(${position + 1}) ${(client.users.cache.get(user.user_id))}: ${user.balance}🍰`
+        let balance = user.balance;
+        if (user.balance.length > 3 && user.balance.length < 7) balance = `${Math.round(user.balance / 100) / 10}k`;
+        else if (user.balance.length > 6 && user.balance.length < 10) balance = `${Math.round(user.balance / 100000) / 10}m`;
+        else if (user.balance.length > 9 && user.balance.length < 13) balance = `${Math.round(user.balance / 100000000) / 10}b`;
+        description += `\n(${position + 1}) ${(client.users.cache.get(user.user_id))}: ${balance}🍰`
       });
     reply(msg.channel.id, description, '#ffffba');
   } else if (command == 'gamble' || command == 'g') {
