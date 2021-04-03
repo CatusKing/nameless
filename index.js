ee90eed08658ee6b4a5ab2aba6379d815e8dc398
CactusKing101 <chalkking17@gmail.com>
2021-04-03 12:31:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ client.on('message', async msg => {
     }
     reply(msg.channel.id, description, '#ffffba');
   } else if (command == 'income') {
-    reply(msg.channel.id, `Ok this is a quick explanation on how points are made on this server. As of when the server first started the two ways to make points goes as follows:\n1. You can make +5🍰 per minute of messaging. This use's a cooldown system that starts a 1 minute cooldown on point gain.\n2. Spending 1 minute in vc will give you +2🍰 point. If you are not muted you will instead get a total of +5🍰 points. If you are not muted and use camera you will get a total +8🍰 points. If you can not use your camera you can instead screenshare while unmuted to get a total of +6🍰 points.\n3. also events may give point eventually`, '#ffffba')
+    reply(msg.channel.id, `Ok this is a quick explanation on how points are made on this server. As of when the server first started the two ways to make points goes as follows:\n1. You can make +5🍰 points per minute of messaging. This use's a cooldown system that starts a 1 minute cooldown on point gain.\n2. Spending 1 minute in vc will give you +2🍰 points. If you are not muted you will instead get a total of +5🍰 points. If you are not muted and use camera you will get a total +8🍰 points. If you can not use your camera you can instead screenshare while unmuted to get a total of +6🍰 points.\n3. also events may give points :D`, '#ffffba')
   } else if (command == 'balance' || command == 'bal') {
     const target = msg.mentions.users.first() || msg.author;
     return reply(msg.channel.id, `${target.tag} has ${currency.getBalance(target.id)}🍰`, '#ffffba');
