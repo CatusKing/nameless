3020af245d07409deca9fe70affd672e7f18c864
CactusKing101 <chalkking17@gmail.com>
2021-04-09 19:12:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ client.once('ready', async () => {
       .replace('%prefix%', prefix)
       .replace('%top%', top)
     );
-  }, 60000);
+  }, 600000);
   setInterval(() => {
     client.channels.cache.get('830198572996624404').messages.fetch('830210843358003270')
       .then(message => {
