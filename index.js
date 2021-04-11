92fed42a33a337eb446314bb313e2e9142b2aed9
CactusKing101 <chalkking17@gmail.com>
2021-04-11 13:33:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -385,4 +385,9 @@ client.on('message', async msg => {
   }
 });
 
+client.on('messageUpdate', (oldMsg, newMsg) => {
+  if (oldMsg.content) log('830856984579670086', `${newMsg.author} just edited a message\nOld: ${oldMsg.content}\nNew: ${newMsg.content}`, '#9e9d9d');
+  else log('830856984579670086', `${newMsg.author} just edited a past message\nNew: ${newMsg.content}`, '#9e9d9d');
+});
+
 client.login(token.main);
