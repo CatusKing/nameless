0016b64b0b96d7c538ab30bf4153dba65e6d5d06
CactusKing101 <chalkking17@gmail.com>
2021-04-12 01:28:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -386,7 +386,6 @@ client.on('message', async msg => {
 });
 
 client.on('messageUpdate', (oldMsg, newMsg) => {
-  if (oldMsg.partial) console.log('partial');
   if (newMsg.author.bot) return;
   if (oldMsg.content) log('830856984579670086', `${newMsg.author} just edited a message\nOld: ${oldMsg.content}\nNew: ${newMsg.content}`, '#9e9d9d');
   else log('830856984579670086', `${newMsg.author} just edited a past message\nNew: ${newMsg.content}`, '#9e9d9d');
