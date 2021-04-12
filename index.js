012587886a0a8a3c8bfc533cf9ee5d9a9b18b084
CactusKing101 <chalkking17@gmail.com>
2021-04-12 01:38:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -388,10 +388,10 @@ client.on('message', async msg => {
 client.on('messageUpdate', (oldMsg, newMsg) => {
   if (oldMsg.partial) {
     try {
-      oldMsg.fetch();
-      newMsg.fetch();
-      if (oldMsg.content) log('830856984579670086', `${newMsg.author} just edited a message\nOld: ${oldMsg.content}\nNew: ${newMsg.content}`, '#9e9d9d');
-      else log('830856984579670086', `${newMsg.author} just edited a past message\nNew: ${newMsg.content}`, '#9e9d9d');
+      oldMsg.fetch().then(fullMessage => {
+        if (fullMessage.content) log('830856984579670086', `${newMsg.author} just edited a message\nOld: ${fullMessage.content}\nNew: ${newMsg.content}`, '#9e9d9d');
+        else log('830856984579670086', `${newMsg.author} just edited a past message\nNew: ${newMsg.content}`, '#9e9d9d');
+      });
     } catch (error) {
       console.error(error);
     }
