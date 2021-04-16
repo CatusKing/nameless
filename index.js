7bc950e50b34be6088c1f64e7ab3fc1197838db7
CactusKing101 <chalkking17@gmail.com>
2021-04-16 19:33:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -478,10 +478,8 @@ client.on('guildMemberAdd', member => {
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     // This is just to simplify the message being sent below (inviter doesn't have a tag property)
     const inviter = client.users.cache.get(invite.inviter.id);
-    // Get the log channel (change to your liking)
-    const logChannel = member.guild.channels.cache.find(channel => channel.name === "joins");
-    // A real basic message with the information we need. 
-    logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
+    
+    log('832758919059341313', `${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`, '#9e9d9d');
   });
 });
 
