fed4522f080169e635151f624f62a472e82f34b2
CactusKing101 <chalkking17@gmail.com>
2021-04-18 13:52:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -314,7 +314,7 @@ client.on('message', async msg => {
 
     if (args[0] == 'all') bet = balance;
     else if (!isNaN(args[0]) && Math.floor(args[0]) >= 500) bet = Math.floor(args[0]);
-    else return reply(msg.channel.id, `Hey sorry but you need to use the command like this ${prefix}gamble <all \\|\\| number \\|\\| help\n>Minimal gamble amount is 500🍰`, '#9e9d9d');
+    else return reply(msg.channel.id, `Hey sorry but you need to use the command like this ${prefix}gamble <all \\|\\| number \\|\\| help>\nMinimal gamble amount is 500🍰`, '#9e9d9d');
     
     if (bet > balance || bet < 500) return reply(msg.channel.id, `Not enough funds! Your balance is ${balance}🍰 You need at least 500🍰`, '#9e9d9d');
     var slot1 = Math.floor(Math.random() * config.emojis.length);
