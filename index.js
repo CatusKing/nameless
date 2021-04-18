0cd604356d9db4494aed7e172ead2a4c91f62cdc
CactusKing101 <chalkking17@gmail.com>
2021-04-18 13:48:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -398,6 +398,7 @@ client.on('message', async msg => {
         msg.member.roles.add(role);
         currency.addBalance(msg.author.id, -config.shop[i][2]);
         currency.addBalance('bank', config.shop[i][2]);
+        log('830503210951245865', `-${config.shop[i][2]}🍰 to ${msg.author} for buying ${role}`, '#ff7784');
         reply(msg.channel.id, `${msg.author} you now have ${role}`, '#ffffba');
         bought = true;
         break;
