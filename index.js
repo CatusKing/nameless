5471ef1a19e030292aa3661ff1bc02ed11b7e7d2
CactusKing101 <chalkking17@gmail.com>
2021-04-20 18:07:53 -0400

    🩹

--- index.js
+++ index.js
@@ -266,14 +266,19 @@ client.on('message', async msg => {
   var reason = [];
   const scores = await get_attrs(msg.content)
   for(let i of ATTRIBUTES) {
-    if (scores[i] >= 0.85) {
+    if (scores[i] >= 0.75) {
       ++warn;
       reason.push(i);
     }
   }
   if (warn == 1) {
-    reply(msg.channel.id, `This is a warning. You have been flagged for the following reason: ${reason[0].toLowerCase()}\nYou had a score of ${scores[reason[0]]}. This has been brought to the moderators attention and will be dealt with accordingly.`, '#ff0000');
+    reply(msg.channel.id, `This is a warning. You have been flagged for the following reason: ${reason[0].toLowerCase()}: ${scores[reason[0]]}\nThis has been brought to the moderators attention and will be dealt with accordingly.`, '#ff0000');
     log('834179033289719839', `Warned\n\nReason:\n${reason[0].toLowerCase()}: ${scores[reason[0]]}\n\nContent:\n${msg.content}\n\n${msg.url}`, '#9e9d9d');
+  } else if (warn == 1 && scores[reason[0]] > 0.90) {
+    const role = client.guilds.cache.get('830495072876494879').roles.cache.get('830495536582361128');
+    msg.member.roles.add(role);
+    reply(msg.channel.id, `You have been muted for the following reason:\n${description}\nThis has been brought to the moderators attention and will be dealt with accordingly.`, '#ff0000');
+    log('834179033289719839', `**Muted**\n\nReason:\n${reason[0].toLowerCase()}: ${scores[reason[0]]}\n\nContent:\n${msg.content}\n\n${msg.url}`, '#9e9d9d');
   } else if (warn > 1) {
     var description = '';
     for(let i of reason) {
@@ -281,7 +286,7 @@ client.on('message', async msg => {
     }
     const role = client.guilds.cache.get('830495072876494879').roles.cache.get('830495536582361128');
     msg.member.roles.add(role);
-    reply(msg.channel.id, `You have been muted for the following reasons:\n${description}\nYour mute will dealt with after further mod actions.`, '#ff0000');
+    reply(msg.channel.id, `You have been muted for the following reasons:\n${description}\nThis has been brought to the moderators attention and will be dealt with accordingly.`, '#ff0000');
     log('834179033289719839', `**Muted**\n\nReasons:\n${description}\n\nContent:\n${msg.content}\n\n${msg.url}`, '#9e9d9d');
   }
 
