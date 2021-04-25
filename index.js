7c9d92685a2fb6b0c456c247b9911fb08203c5c7
CactusKing101 <chalkking17@gmail.com>
2021-04-25 10:18:54 -0400

    ⚡️

--- index.js
+++ index.js
@@ -14,16 +14,11 @@ const prefix = config.prefix;
 var status = 0;
 var invites = [];
 const attributes = ["SEVERE_TOXICITY", "IDENTITY_ATTACK", "THREAT", "SEXUALLY_EXPLICIT"];
+const attrs2 = ["IDENTITY_ATTACK", "THREAT"];
 const tempData = { ignoredCh: data.ignoredCh, admins: data.admins };
-const analyzeRequest = {
-  comment: { text: '' },
-  requestedAttributes: {
-    SEVERE_TOXICITY: {},
-    IDENTITY_ATTACK: {},
-    THREAT: {},
-    SEXUALLY_EXPLICIT: {},
-  },
-};
+const analyzeRequest1 = { comment: { text: '' }, requestedAttributes: { SEVERE_TOXICITY: {} } };
+const analyzeRequest2 = { comment: { text: '' }, requestedAttributes: { IDENTITY_ATTACK: {}, THREAT: {} } };
+const analyzeRequest3 = { comment: { text: '' }, requestedAttributes: { SEXUALLY_EXPLICIT: {} } };
 
 const start = () => {
   Reflect.defineProperty(currency, 'addBalance', {
@@ -183,13 +178,21 @@ const findInvite = (code = String) => {
 
 const get_attrs = async (text) => {
   const app = await google.discoverAPI(config.url);
-  analyzeRequest.comment.text = text;
-  const response = await app.comments.analyze({ key: token.apiKey, resource: analyzeRequest });
+  analyzeRequest1.comment.text = text;
+  analyzeRequest2.comment.text = text;
+  analyzeRequest3.comment.text = text;
+  const response1 = await app.comments.analyze({ key: token.apiKey, resource: analyzeRequest1 });
+  const response2 = await app.comments.analyze({ key: token.apiKey, resource: analyzeRequest2 });
+  const response3 = await app.comments.analyze({ key: token.apiKey, resource: analyzeRequest3 });
   const attrs = {};
-  for (let attr of attributes) {
-    const prediction = response.data["attributeScores"][attr]["summaryScore"]["value"];
+  var prediction = response1.data["attributeScores"]["SEVERE_TOXICITY"]["summaryScore"]["value"];
+  attrs[attr] = prediction;
+  for (let attr of attrs2) {
+    const prediction = response2.data["attributeScores"][attr]["summaryScore"]["value"];
     attrs[attr] = prediction;
   }
+  prediction = response3.data["attributeScores"]["SEXUALLY_EXPLICIT"]["summaryScore"]["value"];
+  attrs[attr] = prediction;
   return attrs;
 };
 
