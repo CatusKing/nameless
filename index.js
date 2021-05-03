867c3f83bbad3c0bd33af5b5a6fffa9bd0d16226
CactusKing101 <chalkking17@gmail.com>
2021-05-03 11:08:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -578,7 +578,6 @@ client.on('presenceUpdate', (presence1, presence2) => {
   if (presence2.user.bot) return;
   var embed = new Discord.MessageEmbed().setColor('#9e9d9d').setTitle(`${presence2.member.displayName}'s Presence`).setDescription(`~ is new`);
   let description = '';
-  if (presence1 && presence2 && presence1.status != presence2.status) embed.addField('Status', `${presence1.status}`, true);
   if (presence1 && presence2 && presence1.clientStatus) embed.addField('Client Status', `Desktop: ${presence1.clientStatus.desktop}\nMobile: ${presence1.clientStatus.mobile}\nWeb: ${presence1.clientStatus.web}\n`, true)
   if (presence1 && presence1.activities) {
     for(let i = 0; i < presence1.activities.length; ++i) {
@@ -589,7 +588,6 @@ client.on('presenceUpdate', (presence1, presence2) => {
     }
     embed.addField('\u200B', '\u200B', false);
   }
-  if (presence1 && presence2 && presence1.status != presence2.status) embed.addField('~Status~', `${presence2.status}`, true);
   if (presence1 && presence2 && presence2.clientStatus) embed.addField('~Client Status~', `Desktop: ${presence2.clientStatus.desktop}\nMobile: ${presence2.clientStatus.mobile}\nWeb: ${presence2.clientStatus.web}\n`, true)
   if (presence2) {
     for(let i = 0; i < presence2.activities.length; ++i) {
