31dd69c925fba73b6ff3366a0cda3e856766c768
CactusKing101 <chalkking17@gmail.com>
2021-05-24 09:25:35 -0400

    Update commands.js

--- general/commands.js
+++ general/commands.js
@@ -21,6 +21,7 @@ const dmCommands = (client = Discord.Client, msg = Discord.Message) => {
 const announcements = (client = Discord.Client, msg = Discord.Message) => {
   const announcementChannel = client.channels.cache.get('830506698908893235');
   const eventChannel = client.channels.cache.get('830506718164287498');
+  const partnerChannel = client.channels.cache.get('846376197163581490');
   if (msg.channel.id == '830503569622827069' && msg.content.includes('!announce!')) {
     if (msg.content.toLowerCase() == 'yes' || msg.content.toLowerCase() == 'no') return;
     msg.channel.send(`Is this announcement ok? (Respond yes or no)\n${msg.content.replace('!announce!', '')}`)
@@ -83,6 +84,40 @@ const announcements = (client = Discord.Client, msg = Discord.Message) => {
               }
             } else {
               msg.channel.send('Event canceled!');
+            }
+          }).catch(() => {
+            msg.channel.send('No response :(');
+          });
+      });
+  }
+  if (msg.channel.id == '830503569622827069' && msg.content.includes('!partner!')) {
+    if (msg.content.toLowerCase() == 'yes' || msg.content.toLowerCase() == 'no') return;
+    msg.channel.send(`Is this partner message ok?\nRespond yes or no\nImage will be included\n\n${msg.content.replace('!partner!', '')}`)
+      .then(async () => {
+        const filter = m => m.author.id == msg.author.id;
+        msg.channel.awaitMessages(filter, { max: 1, time: 15000, errors: ['time'] })
+          .then(async collected => {
+
+            if (collected.first().content.toLowerCase().includes('yes')) {
+              try {
+                const webhooks = await partnerChannel.fetchWebhooks();
+                const webhook = webhooks.first();
+
+                if (webhook == null) return msg.channel.send('Error:\nNo webhooks found!');
+                var embeds = [];
+                for (let i of msg.attachments) {
+                  embeds.push(new Discord.MessageEmbed().setImage(i[1].url).setColor('#9e9d9d'));
+                }
+                await webhook.send(msg.content.replace('!partner!', ''), {
+                  username: msg.guild.name,
+                  avatarURL: msg.guild.iconURL(),
+                  embeds: embeds,
+                });
+              } catch (error) {
+                console.warn(error);
+              }
+            } else {
+              msg.channel.send('Partner message canceled!');
             }
           }).catch(() => {
             msg.channel.send('No response :(');
