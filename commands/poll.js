bc7fda4c473d9bc350a95110182732730eb69903
CactusKing101 <chalkking17@gmail.com>
2021-06-30 01:06:25 -0400

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -37,7 +37,9 @@ module.exports = {
     description += `\`\`\``
     msg.channel.send(embed.setDescription(description)).then((message) => {
       for(let i = 0; i < emojis.length; ++i) {
-        message.react(emojis[i]);
+        message.react(emojis[i]).then(() => {
+          continue;
+        });
       }
       message.awaitReactions((reaction, user) => emojis.includes(reaction.emoji.name), { max: 500, time: 60000 }).then(collected => {
         console.log(collected);
