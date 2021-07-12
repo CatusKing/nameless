eec55c4b0be3297623a1e5f87f71fcc494f09bc5
CactusKing101 <chalkking17@gmail.com>
2021-07-12 17:29:12 -0400

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -4,9 +4,9 @@ module.exports = {
   usage: `leaderboard`,
   command: true,
   aliases: ['leaderboard', 'lb'],
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard) {
+	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     if (msg.member.roles.cache.has('830496065366130709')) {
-      updateLeaderboard();
+      updateLeaderboard(client, db, round);
       reply(msg.channel.id, `Updated the leaderboard`, '#ffffba');
     } else reply(msg.channel.id, `You don't have perms for that you dumb`, '#9e9d9d');
   }
