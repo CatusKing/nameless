c768b38fea215db5b1038d0c167edf435ccd85a6
CactusKing101 <chalkking17@gmail.com>
2021-07-12 21:47:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -397,7 +397,7 @@ client.on('message', async msg => {
         value.execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, client.commands, client.functions.get('updateLeaderboard').execute, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db);
       } catch (err) {
         msg.reply('there was an error trying to execute that command!');
-        console.error(error);
+        console.error(err);
       }
     }
   });
