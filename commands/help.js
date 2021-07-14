5ca5c03ca84f81293bb2af90355c0fb724c042dd
CactusKing101 <chalkking17@gmail.com>
2021-07-14 14:32:46 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,8 +9,8 @@ module.exports = {
 	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands) {
     var embed = new MessageEmbed().setColor('#9e9d9d')
     if (commands.has(args[0]) && commands.get(args[0]).command) {
-      embed.setTitle(commands.get(args[0]).name)
-        .setDescription(`\`\`\`\n${commands.get(args[0]).description}\n\`\`\`\n\`\`\`\n${prefix}${commands.get(args[0]).usage}\n\`\`\``);
+      embed.setTitle(commands.get(args[0].toLowercase()).name)
+        .setDescription(`\`\`\`\n${commands.get(args[0]).description}\n\`\`\`\`\`\`\n${commands.get(args[0].toLowercase()).aliases}\`\`\`\n${prefix}${commands.get(args[0].toLowercase()).usage}\n\`\`\``);
     } else {
       let description = '\`\`\`';
       commands.forEach((value, key) => {
