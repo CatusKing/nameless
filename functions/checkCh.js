ea059a0a8e985d1a37c52c7fa5ea8762f9865a2d
CactusKing101 <chalkking17@gmail.com>
2021-07-14 22:50:17 -0400

    Update checkCh.js

--- functions/checkCh.js
+++ functions/checkCh.js
@@ -1,7 +1,9 @@
+const { adminRoles } = require('../general/config.json');
 module.exports = {
 	execute(client) {
     const videoOnlyCh = client.channels.cache.get('831347288710316032');
     const generalCh = client.channels.cache.get('830495073430929472');
+    const smallRoomCh = client.channels.cache.get('832047338456612885');
     videoOnlyCh.members.forEach(m => {
       if (!m.voice.selfVideo && !m.user.bot) {
         m.voice.setChannel(generalCh, 'Video not enabled in the video only channel');
@@ -18,5 +20,16 @@ module.exports = {
         member.roles.remove(role);
       }
     });
+    var limit = 3;
+    smallRoomCh.members.forEach(m => {
+      let yes = true;
+      m.roles.forEach(r => {
+        if (adminRoles.includes(r.id) && yes) {
+          !yes;
+          ++limit;
+        }
+      });
+    });
+    smallRoomCh.setUserLimit(limit, 'Update because admin left or joined');
   }
 };
