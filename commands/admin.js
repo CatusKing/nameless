dec60c0c82e101fd38d868868a7dc05eb983e9ca
CactusKing101 <chalkking17@gmail.com>
2021-07-14 23:04:22 -0400

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -1,3 +1,4 @@
+const { adminRoles } = require('../general/config.json');
 module.exports = {
 	name: 'admin',
 	description: 'Toggles admin mode',
@@ -5,22 +6,26 @@ module.exports = {
   command: true,
   aliases: ['admin'],
 	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins) {
-		if (msg.member.roles.cache.has('830496065366130709') || msg.member.roles.cache.has('830495937301577759')) {
-      if (admins.includes(msg.author.id)) {
-        for (var i = 0; i < admins.length; i++) {
-
-          if (admins[i] == msg.author.id) {
-            admins.splice(i, 1);
-            reply(msg.channel.id, `No longer ignoring you from auto mod\nid: ${msg.author.id}`, '#9e9d9d');
-            break;
+		let yes = true;
+    msg.member.roles.cache.forEach(r => {
+      if (adminRoles.includes(r.id) && yes) {
+        if (admins.includes(msg.author.id)) {
+          for (var i = 0; i < admins.length; i++) {
+  
+            if (admins[i] == msg.author.id) {
+              admins.splice(i, 1);
+              reply(msg.channel.id, `No longer ignoring you from auto mod\nid: ${msg.author.id}`, '#9e9d9d');
+              break;
+            }
           }
+        } else {
+          admins.push(msg.author.id);
+          reply(msg.channel.id, `Ignoring you from auto mod\nid: ${msg.author.id}`, '#9e9d9d');
         }
-      } else {
-        admins.push(msg.author.id);
-        reply(msg.channel.id, `Ignoring you from auto mod\nid: ${msg.author.id}`, '#9e9d9d');
+        setServerAdmins(admins);
+        return admins;
       }
-      setServerAdmins(admins);
-			return admins;
-    } else return reply(msg.channel.id, `Sorry you don't have perms for this`, '#9e9d9d');
+    });
+    if (yes) reply(msg.channel.id, `Sorry you don't have perms for this`, '#9e9d9d');
 	},
 };
