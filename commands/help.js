aaeca6e81adfbbaa8206972d4384b0e0617bfb0d
CactusKing101 <chalkking17@gmail.com>
2021-07-14 22:50:19 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,4 @@
-const { Collection, MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const { prefix } = require('../general/config.json');
 module.exports = {
 	name: 'help',
