7599ce378dde79561a1932393ee60821995425ad
CactusKing101 <chalkking17@gmail.com>
2021-07-31 11:12:58 -0400

    Update wikihow.js

--- commands/wikihow.js
+++ commands/wikihow.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const jsdom = require("jsdom");
 module.exports = {
   name: 'wikihow',
   description: 'Retrieves a random WikiHow image',
@@ -8,9 +8,8 @@ module.exports = {
   execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     request('http://www.wikihow.com/Special:Randomizer', { json: false }, (err, res, body) => {
       if (err) console.warn(err);
-      var parser = new DOMParser();
-      var doc = parser.parseFromString(body, 'text/html');
-      console.log(doc.querySelector('title'));
+      const dom = new jsdom.JSDOM(body);
+      console.log(dom.window.document.querySelector("title").textContent);
     });
   }
 };
