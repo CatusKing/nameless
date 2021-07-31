58d34fcc66a6c1a6f5423b3f469d3906bfb9321f
CactusKing101 <chalkking17@gmail.com>
2021-07-31 11:33:51 -0400

    Update wikihow.js

--- commands/wikihow.js
+++ commands/wikihow.js
@@ -7,14 +7,11 @@ module.exports = {
   usage: `wikihow`,
   command: true,
   aliases: ['wikihow'],
-  execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+  execute(client, msg, args, reply) {
     request('http://www.wikihow.com/Special:Randomizer', { json: false }, (err, res, body) => {
       if (err) console.warn(err);
       const dom = new jsdom.JSDOM(body);
-      console.log(dom.window.document.querySelector("title").textContent);
-      msg.channel.send(dom.window.document.querySelector("title").textContent);
-      console.log(`https://www.wikihow.com/images/thumb/b/b2/${dom.window.document.querySelector("title").textContent.replace(/[# ]/g, '-')}-2.jpg/v4-460px-${dom.window.document.querySelector("title").textContent.replace(/[# ]/g, '-')}-2.jpg`);
-      msg.channel.send(new MessageEmbed().setImage(`https://www.wikihow.com/images/thumb/b/b2/${dom.window.document.querySelector("title").textContent.replace(/[# ]/g, '-')}-2.jpg/v4-460px-${dom.window.document.querySelector("title").textContent.replace(/[# ]/g, '-')}-2.jpg`))
+      reply(msg.channel.id, dom.window.document.querySelector("title").textContent);
     });
   }
 };
