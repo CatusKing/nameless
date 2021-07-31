d5245e09a3df28764cdee3f0bbeb856ce054f04c
CactusKing101 <chalkking17@gmail.com>
2021-07-31 01:28:52 -0400

    Update wikihow.js

--- commands/wikihow.js
+++ commands/wikihow.js
@@ -6,7 +6,7 @@ module.exports = {
   command: true,
   aliases: ['wikihow'],
 	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
-    request('https://hargrimm-wikihow-v1.p.rapidapi.com/images?count=3', { headers: { "method": "GET", "x-rapidapi-host": "hargrimm-wikihow-v1.p.rapidapi.com" }, json: true }, (err, res, body) => {
+    request('http://www.wikihow.com/Special:Randomizer', { json: true }, (err, res, body) => {
       if (err) console.warn(err);
       console.log(body);
     });
