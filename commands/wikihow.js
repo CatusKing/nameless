48b5e44a6c3bbcf1b6cd895716f4c1a4efa3598b
CactusKing101 <chalkking17@gmail.com>
2021-07-31 01:18:32 -0400

    Create wikihow.js

---
+++ commands/wikihow.js
@@ -0,0 +1,14 @@
+const request = require('request');
+module.exports = {
+	name: 'wikihow',
+	description: 'Retrieves a random WikiHow image',
+  usage: `wikihow`,
+  command: true,
+  aliases: ['wikihow'],
+	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    request('https://hargrimm-wikihow-v1.p.rapidapi.com/images?count=3', { headers: {'x-rapidapi-host': 'hargrimm-wikihow-v1.p.rapidapi.com'}, json: true }, (err, res, body) => {
+      if (err) console.warn(err);
+      console.log(body);
+    });
+  }
+};
