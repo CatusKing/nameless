1573a42469f404813029d414e921745a2a4b64a5
CactusKing101 <chalkking17@gmail.com>
2021-08-21 13:47:15 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,10 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
 	name: 'invite',
 	description: 'Sends the invite link',
 	usage: `invite`,
 	command: true,
 	aliases: ['invite'],
+  slash: true,
+  options: [],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor) {
+    interaction.reply({ embeds: [new MessageEmbed().setDescription(`[discord.gg/Hja2gSnsAu](https://discord.gg/Hja2gSnsAu)`).setColor('#ffffba')] })
+  },
 	execute(client, msg, args, reply) {
-    reply(msg.channel.id, `[Hja2gSnsAu](https://discord.gg/Hja2gSnsAu)`, '#ffffba');
+    reply(msg.channel.id, `[discord.gg/Hja2gSnsAu](https://discord.gg/Hja2gSnsAu)`, '#ffffba');
   }
 };
