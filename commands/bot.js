e5d461b7b5bf4edb1f17b9c9c6bcb19a5f9d4158
CactusKing101 <chalkking17@gmail.com>
2021-08-21 14:29:53 -0400

    Update bot.js

--- commands/bot.js
+++ commands/bot.js
@@ -43,7 +43,7 @@ module.exports = {
     }
     const embed = new MessageEmbed().setColor('#ffffba')
       .addField('General', `**❯ ${nameless_emoji}** ${client.user.tag} (${client.user.id})\n**❯ ${pc_emoji}** ${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}hr ${Math.floor(client.uptime / 60000) % 60}min ${Math.floor(client.uptime / 1000) % 60}sec\n\u200b`)
-      .addField('System', `**❯ ${ping_emoji}:** ${Math.round(client.ws.ping)}ms ws ping\n**❯ ${cpu_emoji}:**\n\u3000 ${core.model}\n\u3000 ${os.cpus().length} x ${core.speed}MHz\n**❯ ${ram_emoji}:** ${Math.floor(((os.freemem()) / 10000000)) / 100}GiB/${Math.floor(((os.totalmem()) / 10000000)) / 100}GiB\n**❯ Dependencies:**\n\u3000 Node.js: ${process.version}\n\u3000 Discord.js: v${version1}\n\u3000 os: v0.1.1\n\u3000 quick.db: v${version2}\n\u3000 Request: v2.88.2\n\u3000 googleapis: v71.0.0\n\u200b`)
+      .addField('System', `**❯ ${ping_emoji}:** ${Math.round(client.ws.ping)}ms ws ping\n**❯ ${hdd_emoji}:** ${os.platform()},${os.release()}\n**❯ ${cpu_emoji}:**\n\u3000 ${core.model}\n\u3000 ${os.cpus().length} x ${core.speed}MHz\n**❯ ${ram_emoji}:** ${Math.floor(((os.freemem()) / 10000000)) / 100}GiB/${Math.floor(((os.totalmem()) / 10000000)) / 100}GiB\n**❯ Dependencies:**\n\u3000 Node.js: ${process.version}\n\u3000 Discord.js: v${version1}\n\u3000 os: v0.1.1\n\u3000 quick.db: v${version2}\n\u3000 Request: v2.88.2\n\u3000 googleapis: v71.0.0\n\u200b`)
       .addField('Current Apis', api)
       .setTimestamp();
     msg.channel.send({ embeds: [embed] });
