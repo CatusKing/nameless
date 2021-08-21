6c7ee2764138f07be885ad86bdbd3dadc04b8f5a
CactusKing101 <chalkking17@gmail.com>
2021-08-21 14:29:32 -0400

    Update bot.js

--- commands/bot.js
+++ commands/bot.js
@@ -18,28 +18,14 @@ module.exports = {
     const ping_emoji = client.emojis.cache.get("864314397529866261");
     const nameless_emoji = client.emojis.cache.get("864319487746113557");
     const core = os.cpus()[0];
-    console.log(apis)
+    var api = '';
+    for(let i of apis) {
+      api += `${i}\n`;
+    }
     const embed = new MessageEmbed().setColor('#ffffba')
-      .addField('General', [
-        `**❯ ${nameless_emoji}** ${client.user.tag} (${client.user.id})`,
-        `**❯ ${pc_emoji}** ${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}hr ${Math.floor(client.uptime / 60000) % 60}min ${Math.floor(client.uptime / 1000) % 60}sec`,
-      ])
-      .addField('System', [
-        `**❯ ${ping_emoji}:** ${Math.round(client.ws.ping)}ms ws ping`,
-        `**❯ ${hdd_emoji}:** ${os.platform()},${os.release()}`,
-        `**❯ ${cpu_emoji}:**`,
-        `\u3000 ${core.model}`,
-        `\u3000 ${os.cpus().length} x ${core.speed}MHz`,
-        `**❯ ${ram_emoji}:** ${Math.floor(((os.freemem()) / 10000000)) / 100}GiB/${Math.floor(((os.totalmem()) / 10000000)) / 100}GiB`,
-        `**❯ Dependencies:**`,
-        `\u3000 Node.js: ${process.version}`,
-        `\u3000 Discord.js: v${version1}`,
-        `\u3000 os: v0.1.1`,
-        `\u3000 quick.db: v${version2}`,
-        `\u3000 Request: v2.88.2`,
-        `\u3000 googleapis: v71.0.0`,
-      ])
-      .addField('Current Apis', apis)
+      .addField('General', `**❯ ${nameless_emoji}** ${client.user.tag} (${client.user.id})\n**❯ ${pc_emoji}** ${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}hr ${Math.floor(client.uptime / 60000) % 60}min ${Math.floor(client.uptime / 1000) % 60}sec\n\u200b`)
+      .addField('System', `**❯ ${ping_emoji}:** ${Math.round(client.ws.ping)}ms ws ping\n**❯ ${hdd_emoji}:** ${os.platform()},${os.release()}\n**❯ ${cpu_emoji}:**\n\u3000 ${core.model}\n\u3000 ${os.cpus().length} x ${core.speed}MHz\n**❯ ${ram_emoji}:** ${Math.floor(((os.freemem()) / 10000000)) / 100}GiB/${Math.floor(((os.totalmem()) / 10000000)) / 100}GiB\n**❯ Dependencies:**\n\u3000 Node.js: ${process.version}\n\u3000 Discord.js: v${version1}\n\u3000 os: v0.1.1\n\u3000 quick.db: v${version2}\n\u3000 Request: v2.88.2\n\u3000 googleapis: v71.0.0\n\u200b`)
+      .addField('Current Apis', api)
       .setTimestamp();
     interaction.reply({ embeds: [embed] });
   },
@@ -51,29 +37,14 @@ module.exports = {
     const ping_emoji = client.emojis.cache.get("864314397529866261");
     const nameless_emoji = client.emojis.cache.get("864319487746113557");
     const core = os.cpus()[0];
+    var api = '';
+    for(let i of apis) {
+      api += `${i}\n`;
+    }
     const embed = new MessageEmbed().setColor('#ffffba')
-      .addField('General', [
-        `**❯ ${nameless_emoji}** ${client.user.tag} (${client.user.id})`,
-        `**❯ ${pc_emoji}** ${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}hr ${Math.floor(client.uptime / 60000) % 60}min ${Math.floor(client.uptime / 1000) % 60}sec`,
-        '\u200b'
-      ])
-      .addField('System', [
-        `**❯ ${ping_emoji}:** ${Math.round(client.ws.ping)}ms ws ping`,
-        `**❯ ${hdd_emoji}:** ${os.platform()},${os.release()}`,
-        `**❯ ${cpu_emoji}:**`,
-        `\u3000 ${core.model}`,
-        `\u3000 ${os.cpus().length} x ${core.speed}MHz`,
-        `**❯ ${ram_emoji}:** ${Math.floor(((os.freemem()) / 10000000)) / 100}GiB/${Math.floor(((os.totalmem()) / 10000000)) / 100}GiB`,
-        `**❯ Dependencies:**`,
-        `\u3000 Node.js: ${process.version}`,
-        `\u3000 Discord.js: v${version1}`,
-        `\u3000 os: v0.1.1`,
-        `\u3000 quick.db: v${version2}`,
-        `\u3000 Request: v2.88.2`,
-        `\u3000 googleapis: v71.0.0`,
-        '\u200b'
-      ])
-      .addField('Current Apis', apis)
+      .addField('General', `**❯ ${nameless_emoji}** ${client.user.tag} (${client.user.id})\n**❯ ${pc_emoji}** ${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}hr ${Math.floor(client.uptime / 60000) % 60}min ${Math.floor(client.uptime / 1000) % 60}sec\n\u200b`)
+      .addField('System', `**❯ ${ping_emoji}:** ${Math.round(client.ws.ping)}ms ws ping\n**❯ ${cpu_emoji}:**\n\u3000 ${core.model}\n\u3000 ${os.cpus().length} x ${core.speed}MHz\n**❯ ${ram_emoji}:** ${Math.floor(((os.freemem()) / 10000000)) / 100}GiB/${Math.floor(((os.totalmem()) / 10000000)) / 100}GiB\n**❯ Dependencies:**\n\u3000 Node.js: ${process.version}\n\u3000 Discord.js: v${version1}\n\u3000 os: v0.1.1\n\u3000 quick.db: v${version2}\n\u3000 Request: v2.88.2\n\u3000 googleapis: v71.0.0\n\u200b`)
+      .addField('Current Apis', api)
       .setTimestamp();
     msg.channel.send({ embeds: [embed] });
   }
