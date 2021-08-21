c28fc1393ab8d2d22fe85e3e6930c01692e1c75e
CactusKing101 <chalkking17@gmail.com>
2021-08-21 01:36:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const db = require('quick.db');
 const { google } = require('googleapis');
 const fs = require('fs');
 const request = require('request');
-
-const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'], ws: { properties: { $browser: "Discord iOS" }, intents: new Intents(32767) } });
+const intents = new Intents(32767);
+const client = new Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'], ws: { properties: { $browser: "Discord iOS" }, intents: intents } });
 const prefix = config.prefix;
 var status = 0;
 var invites = [];
