1e45ed0773f685fa5f22b9faff10f4ae08c10445
CactusKing101 <chalkking17@gmail.com>
2021-08-22 19:38:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -347,7 +347,7 @@ client.once('ready', async () => {
     }
   });
   client.application.commands.set(commands, config.guildId);
-  client.application.commands.delete('879146992746520626', config.guildId)
+  client.application.commands.delete(client.application.commands.cache.get('879146992746520626'), config.guildId)
   console.log('Finished setting up slash commands');
 
   console.log(`Logged in as ${client.user.tag}`);
