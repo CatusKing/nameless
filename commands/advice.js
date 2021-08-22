5c7b233c11f292c88adeb7c6d1fea433aaa17004
CactusKing101 <chalkking17@gmail.com>
2021-08-22 18:15:27 -0400

    Update advice.js

--- commands/advice.js
+++ commands/advice.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const request = require('request');
 module.exports = {
 	name: 'advice',
@@ -5,6 +6,14 @@ module.exports = {
   usage: `advice`,
   command: true,
   aliases: ['advice'],
+  slash: true,
+  options: [],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    request(`https://api.adviceslip.com/advice`, { json: true }, (err, res, body) => {
+      if (err) return console.log(err);
+      interaction.reply({ embeds: [ new MessageEmbed().setDescription(body.slip.advice).setColor('#9e9d9d') ] });
+    });
+  },
 	execute(client, msg, args, reply) {
     request(`https://api.adviceslip.com/advice`, { json: true }, (err, res, body) => {
       if (err) return console.log(err);
