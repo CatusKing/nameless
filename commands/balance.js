350f89dcde07fc0e5545167e244162752851e5a1
CactusKing101 <chalkking17@gmail.com>
2021-08-22 17:57:47 -0400

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -7,9 +7,16 @@ module.exports = {
   command: true,
   aliases: ['balance', 'bal'],
   slash: true,
-  options: [],
+  options: [
+    {
+      name: 'user',
+      type: 'USER',
+      description: 'The person you want to check the balance of',
+      required: false
+    }
+  ],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
-    const target = msg.mentions.users.first() || msg.author;
+    const target = interaction.user;
     interaction.reply({ embeds: [ new MessageEmbed().setDescription(`${target} has ${floor(getUserBalance(target.id))}🦴(${getUserBalance(target.id)}🦴)`).setColor('#ffffba') ] });
   },
 	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor) {
