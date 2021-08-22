462533f3e63a5cbe1fedb2562bd87b4b968a4539
CactusKing101 <chalkking17@gmail.com>
2021-08-22 18:39:20 -0400

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const config = require('../general/config.json');
 const prefix = config.prefix;
 module.exports = {
@@ -6,6 +7,72 @@ module.exports = {
 	usage: `buy <item>`,
 	command: true,
 	aliases: ['buy'],
+	slash: true,
+	options: [
+		{
+			name: 'items',
+			description: 'What you want to buy',
+			require: true,
+			type: 'STRING',
+			choices: [
+				{
+					name: 'Vip',
+					value: 'vip'
+				},
+				{
+					name: 'DJ',
+					value: 'dj'
+				},
+				{
+					name: 'Keycard',
+					value: 'keycard'
+				},
+				{
+					name: 'Mathematics',
+					value: 'mathematics'
+				},
+				{
+					name: 'Hue',
+					value: 'hue'
+				}
+			]
+		}
+	],
+	executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+		const balance = getUserBalance(interaction.user.id);
+
+		for (let i = 0; i < config.shop.length; ++i) {
+			if (interaction.options.getString('items') == config.shop[i][1]) {
+				const role = interaction.guild.roles.cache.get(config.shop[i][3]);
+
+				if (balance < config.shop[i][2]) {
+					interaction.reply({ embeds: [ new MessageEmbed().setDescription(`You don't have enough funds for the ${role} role\nYou need ${config.shop[i][2]}🦴\nYou have ${balance}🦴`).setColor('#9e9d9d') ] });
+					break;
+				}
+
+				if (config.shop[i][4] == 0) {
+					if (interaction.member.roles.cache.has(config.shop[i][3])) {
+						interaction.reply({ embeds: [ new MessageEmbed().setDescription(`You already have ${role} you dumb`).setColor('#9e9d9d') ] });
+						break;
+					}
+					interaction.member.roles.add(role, `Bought ${role.name}`);
+					log('830503210951245865', `-${config.shop[i][2]}🦴 to ${msg.author} for buying ${role}`, '#ff7784');
+					interaction.reply({ embeds: [ new MessageEmbed().setDescription(`You spent ${config.shop[i][2]}🦴\n${msg.author}, you now have ${role}`).setColor('#9e9d9d') ] });
+				} else {
+					if (!interaction.member.roles.cache.has(config.shop[i][3])) {
+						interaction.reply({ embeds: [ new MessageEmbed().setDescription(`You don't have ${role} you dumb`).setColor('#9e9d9d') ] });
+						break;
+					}
+					interaction.member.roles.remove(role, `Paid to remove ${role.name}`);
+					log('830503210951245865', `-${config.shop[i][2]}🦴 to ${msg.author} for removing ${role}`, '#ff7784');
+					interaction.reply({ embeds: [ new MessageEmbed().setDescription(`You spent ${config.shop[i][2]}🦴\n${msg.author}, you now have removed ${role}`).setColor('#9e9d9d') ] });
+				}
+				addUserBalance(msg.author.id, -config.shop[i][2]);
+				addUserBalance('bank', config.shop[i][2]);
+				break;
+			}
+		}
+	},
 	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance) {
     const balance = getUserBalance(msg.author.id);
 
