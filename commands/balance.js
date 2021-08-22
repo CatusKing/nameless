b65bf3914370c0cc0953cc4999f344646fb55587
CactusKing101 <chalkking17@gmail.com>
2021-08-22 17:37:35 -0400

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,11 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
 	name: 'balance',
 	description: 'Allows you to look at your or another users balance',
   usage: `balance [@User]`,
   command: true,
   aliases: ['balance', 'bal'],
+  slash: true,
+  options: [],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    const target = msg.mentions.users.first() || msg.author;
+    interaction.reply({ embeds: [ new MessageEmbed().setDescription(`${target} has ${floor(getUserBalance(target.id))}🦴(${getUserBalance(target.id)}🦴)`).setColor('#ffffba') ] });
+  },
 	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor) {
     const target = msg.mentions.users.first() || msg.author;
-    return reply(msg.channel.id, `${target} has ${floor(getUserBalance(target.id))}🦴(${getUserBalance(target.id)}🦴)`, '#ffffba');
+    reply(msg.channel.id, `${target} has ${floor(getUserBalance(target.id))}🦴(${getUserBalance(target.id)}🦴)`, '#ffffba');
   }
 };
