1a141cfddd7d8db760556dd23152d5f207719eed
CactusKing101 <chalkking17@gmail.com>
2021-08-22 21:08:54 -0400

    Update shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const config = require('../general/config.json');
 module.exports = {
 	name: 'shop',
@@ -5,6 +6,13 @@ module.exports = {
 	usage: `shop`,
 	command: true,
 	aliases: ['shop'],
+	slash: true,
+	options: [],
+	executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+		var description = '';
+		for (let i = 0; i < config.shop.length; ++i) description += `\n${config.shop[i][0]}`;
+		interaction.reply({ embeds: [ new MessageEmbed().setDescription(description).setColor('#9e9d9d') ] });
+	},
 	execute(client, msg, args, reply) {
 		var description = '';
 		for (let i = 0; i < config.shop.length; ++i) description += `\n${config.shop[i][0]}`;
