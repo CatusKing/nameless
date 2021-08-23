9a1ac3e0ca01c1aab166431feb5a437e7b4a6c00
CactusKing101 <chalkking17@gmail.com>
2021-08-22 20:24:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -420,7 +420,6 @@ client.on('messageCreate', async (msg) => {
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
-  console.log(interaction.commandId)
 
   var admins = getServerAdmins();
   var ignoredCh = getServerIgnoredCh();
