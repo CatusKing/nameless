ec89713bc922ec4d7a6d9dab8cc17527f4353622
CactusKing101 <chalkking17@gmail.com>
2021-08-23 18:19:30 -0400

    Create dog.js

---
+++ commands/dog.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+const request = require("request");
+
+module.exports = {
+	name: 'fox',
+	description: 'Sends a random fox image',
+	usage: `fox`,
+	command: true,
+	aliases: ['fox'],
+  slash: true,
+  options: [],
+  executeI(client, interaction) {
+    request('https://dog.ceo/api/breeds/image/random', { json: true }, (err, res, body) => {
+      interaction.reply({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setImage(body.message) ] });
+    });
+  },
+	execute(client, msg) {
+    request('https://dog.ceo/api/breeds/image/random', { json: true }, (err, res, body) => {
+      msg.channel.send({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setImage(body.message) ] });
+    });
+  }
+};
