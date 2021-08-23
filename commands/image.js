433f177f4d48f3992c5bb2e605625e8d16a8b522
CactusKing101 <chalkking17@gmail.com>
2021-08-23 17:42:00 -0400

    Update image.js

--- commands/image.js
+++ commands/image.js
@@ -7,10 +7,54 @@ module.exports = {
 	command: true,
 	aliases: ['image'],
   slash: true,
-  options: [],
+  options: [
+    {
+      type: 'BOOLEAN',
+      name: 'grayscale',
+      description: 'Whether to make the image grayscale',
+      required: false
+    },
+    {
+      type: 'INTEGER',
+      name: 'blur',
+      description: 'The level of blurring to the image',
+      required: false,
+      choices: [
+        {
+          name: '0',
+          value: 0,
+        },
+        {
+          name: '1',
+          value: 1,
+        },
+        {
+          name: '2',
+          value: 2,
+        },
+        {
+          name: '3',
+          value: 3,
+        },
+        {
+          name: '4',
+          value: 4,
+        },
+        {
+          name: '5',
+          value: 5,
+        },
+      ]
+    }
+  ],
   executeI(client, interaction) {
     var seed = Math.floor(Math.random() * 5000);
-    interaction.reply({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setImage(`https://picsum.photos/seed/${seed}/500/300`) ] })
+    var blur = interaction.options.getInteger('blur') || 0;
+    blur = blur * 2;
+    var input = interaction.options.getBoolean('grayscale') || false;
+    if (input) var grayscale = '&grayscale';
+    else var grayscale = '';
+    interaction.reply({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setImage(`https://picsum.photos/seed/${seed}/500/300?blur=${blur}${grayscale}`) ] })
   },
 	execute(client, msg) {
     var seed = Math.floor(Math.random() * 5000);
