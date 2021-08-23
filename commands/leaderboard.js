e4868144fc0f8faae7876849749bccde09722866
CactusKing101 <chalkking17@gmail.com>
2021-08-22 20:09:40 -0400

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,15 +1,21 @@
-const { MessageEmbed } = require("discord.js");
-
 module.exports = {
 	name: 'leaderboard',
 	description: 'Updates the leaderboard',
   usage: `leaderboard`,
   command: true,
   aliases: ['leaderboard', 'lb'],
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+  slash: true,
+  options: [],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     if (msg.member.roles.cache.has('830496065366130709')) {
       updateLeaderboard(client, db, round);
       interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Updated the leaderboard`).setColor('#ffffba') ] });
     } else interaction.reply({ embeds: [ new MessageEmbed().setDescription(`You don't have perms for that you dumb`).setColor('#ffffba') ] });
+  },
+	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    if (msg.member.roles.cache.has('830496065366130709')) {
+      updateLeaderboard(client, db, round);
+      reply(msg.channel.id, `Updated the leaderboard`, '#ffffba');
+    } else reply(msg.channel.id, `You don't have perms for that you dumb`, '#9e9d9d');
   }
 };
