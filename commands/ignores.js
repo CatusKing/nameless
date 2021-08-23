0e0a38896f3ed6ba0182c4fcaccadc52e5dc3f83
CactusKing101 <chalkking17@gmail.com>
2021-08-22 20:00:24 -0400

    Update ignores.js

--- commands/ignores.js
+++ commands/ignores.js
@@ -1,9 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
 	name: 'ignores',
 	description: 'Displays the current channels ignored from auto-mod',
   usage: `ignores`,
   command: true,
   aliases: ['ignores'],
+  slash: true,
+  options: [],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    if (interaction.member.roles.cache.has('830496065366130709') || interaction.member.roles.cache.has('830495937301577759')) {
+      var description = 'Ignored channels\n';
+      for (let i of ignoredCh) {
+        description += `${client.channels.cache.get(i).name} - ${i}\n`
+      }
+      interaction.reply({ embeds: [ new MessageEmbed().setDescription(description).setColor('#9e9d9d') ] });
+    } else interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Sorry you don't have perms for this`).setColor('#9e9d9d') ] });
+  },
 	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh) {
     if (msg.member.roles.cache.has('830496065366130709') || msg.member.roles.cache.has('830495937301577759')) {
       var description = 'Ignored channels\n';
