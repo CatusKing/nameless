3f61fd3ae7c5c3d4317e759bca73bbf5ac6c3672
CactusKing101 <chalkking17@gmail.com>
2021-08-22 20:07:34 -0400

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
 	name: 'leaderboard',
 	description: 'Updates the leaderboard',
@@ -7,7 +9,7 @@ module.exports = {
 	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     if (msg.member.roles.cache.has('830496065366130709')) {
       updateLeaderboard(client, db, round);
-      reply(msg.channel.id, `Updated the leaderboard`, '#ffffba');
-    } else reply(msg.channel.id, `You don't have perms for that you dumb`, '#9e9d9d');
+      interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Updated the leaderboard`).setColor('#ffffba') ] });
+    } else interaction.reply({ embeds: [ new MessageEmbed().setDescription(`You don't have perms for that you dumb`).setColor('#ffffba') ] });
   }
 };
