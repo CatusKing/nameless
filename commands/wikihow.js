e931748981cd5f55ff88e17b71f05ce5536a9031
CactusKing101 <chalkking17@gmail.com>
2021-08-22 21:37:49 -0400

    Update wikihow.js

--- commands/wikihow.js
+++ commands/wikihow.js
@@ -7,6 +7,27 @@ module.exports = {
   usage: `wikihow`,
   command: true,
   aliases: ['wikihow', 'wh'],
+  slash: true,
+  options: [],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    request('http://www.wikihow.com/Special:Randomizer', { json: false }, (err, res, body) => {
+      if (err) console.warn(err);
+      const dom = new jsdom.JSDOM(body);
+      var yes = true;
+      const embed = new MessageEmbed().setDescription(`[link](https://www.wikihow.com${res.socket._httpMessage.path})\n${dom.window.document.querySelector("title").textContent}`).setColor('#9e9d9d');
+      dom.window.document.querySelectorAll('img').forEach((value) => {
+        if (value.src.includes('https://') && value.src.includes(dom.window.document.querySelector("title").textContent.split(' ')[0]) && yes) {
+          yes = false;
+          embed.setImage(value.src).setDescription(`${embed.description}\n[image](${value.src})`);
+        }
+      });
+      if (embed.description == null) {
+        interaction.reply(`idk dude the code is scuffed(error)`);
+      } else {
+        interaction.reply({ embeds: [embed] });
+      }
+    });
+  },
   execute(client, msg, args, reply) {
     request('http://www.wikihow.com/Special:Randomizer', { json: false }, (err, res, body) => {
       if (err) console.warn(err);
