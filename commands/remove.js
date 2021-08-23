37262c5aab5e28d162c0409e8369336dd5533d58
CactusKing101 <chalkking17@gmail.com>
2021-08-22 20:59:17 -0400

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const { prefix } = require('../general/config.json');
 module.exports = {
 	name: 'remove',
@@ -5,6 +6,32 @@ module.exports = {
   usage: `remove <amount> [@User]`,
   command: true,
   aliases: ['remove'],
+  slash: true,
+  options: [
+    {
+      name: 'amount',
+      type: 'INTEGER',
+      description: 'The amount of 🦴 to add to a user',
+      required: true
+    },
+    {
+      name: 'user',
+      type: 'USER',
+      description: 'The person you want to give 🦴 to',
+      required: false
+    }
+  ],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    if (interaction.member.roles.cache.has('830496065366130709')) {
+      const target = interaction.options.getUser('user') || interaction.user;
+  
+      const amount = interaction.options.getUser('user');
+      const balance = addUserBalance(target.id, -amount);
+      addUserBalance('bank', amount);
+      interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Taken ${amount}🦴 from ${target}\nThey now have ${balance}🦴`).setColor('#9e9d9d') ] });
+      log('830503210951245865', `+${amount}🦴 to ${target} given by ${interaction.user}`, '#ff7784');
+    } else interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Sorry you don't have perms for this`).setColor('#9e9d9d') ] });
+  },
 	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance) {
     if (msg.member.roles.cache.has('830496065366130709')) {
       const target = msg.mentions.users.first() || msg.author;
