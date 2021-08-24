73aec9ae6bce158c5f29061ad3645db527b72175
CactusKing101 <chalkking17@gmail.com>
2021-08-23 20:14:20 -0400

    Create bored.js

---
+++ commands/bored.js
@@ -0,0 +1,16 @@
+const { MessageEmbed } = require("discord.js");
+const request = require("request");
+
+module.exports = {
+	name: 'bored',
+	description: 'Sends a random thing to do',
+	usage: `bored`,
+	command: false,
+  slash: true,
+  options: [],
+  executeI(client, interaction) {
+    request('http://www.boredapi.com/api/activity/', { json: true }, (err, res, body) => {
+      interaction.reply({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setDescription(`\`\`\`\ntype: ${body.type}\nparticipants: ${body.participants}\`\`\`${body.activity}`) ] });
+    });
+  },
+};
