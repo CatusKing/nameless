62822d7d452158c6f0f0f25d2d6b1e5d88bee702
spoon319 <joannareichle@gmail.com>
2021-08-24 18:16:16 -0400

    Update flag.js

--- commands/flag.js
+++ commands/flag.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageActionRow, MessageSelectMenu } = require("discord.js");
 const { flags } = require('../general/config.json');
 
 module.exports = {
@@ -9,7 +9,33 @@ module.exports = {
   slash: true,
   options: [],
   executeI(client, interaction) {
-    var random = Math.floor(flags.length * Math.random())
-    interaction.reply({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setTitle(flags[random][1]).setImage(`https://www.countryflags.io/${flags[random][0]}/flat/64.png`) ] });
+    var random = Math.floor(flags.length * Math.random());
+    var options = [{ label: flags[random][1], value: `${flags[random][0]}-${flags[random][0]}` }];
+    for(let i = 0; i < 9; ++i) {
+      var random2 = Math.floor(flags.length * Math.random());
+      options.push({ label: flags[random2][1], value: `${flags[random2][0]}-${flags[random][0]}` });
+    }
+    const row = new MessageActionRow()
+      .addComponents(
+        new MessageSelectMenu()
+          .setCustomId('flagSelect')
+          .setPlaceholder('Nothing selected')
+          .addOptions([
+            {
+              label: 'Select me',
+              description: 'This is a description',
+              value: 'first_option',
+            },
+            {
+              label: 'You can select me too',
+              description: 'This is also a description',
+              value: 'second_option',
+            },
+          ]),
+      );
+    interaction.reply({ 
+      embeds: [new MessageEmbed().setColor('#9e9d9d').setTitle('What country is this?').setImage(`https://www.countryflags.io/${flags[random][0]}/flat/64.png`)],
+      components: [row]
+    });
   }
 };
