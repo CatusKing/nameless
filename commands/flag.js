5b75b40744bb6ddc353c31edc93195dd24cdd9c2
CactusKing101 <chalkking17@gmail.com>
2021-08-25 00:04:11 -0400

    Update flag.js

--- commands/flag.js
+++ commands/flag.js
@@ -35,7 +35,7 @@ module.exports = {
     var guess = interaction.values[0].split('-')[0];
     var answer = interaction.values[0].split('-')[1];
     interaction.deleteReply();
-    if (guess == answer) interaction.followUp({ embeds: [new MessageEmbed().setColor('#baffc9').setDescription('Pog you got it right!\n\n(rn you don\'t get money for this yet)')], components: [] });
-    else interaction.followUp({ embeds: [new MessageEmbed().setColor('#ff7784').setDescription('You got it wrong :(\n\n(rn you don\'t get money for this yet)')], components: [] });
+    if (guess == answer) interaction.reply({ embeds: [new MessageEmbed().setColor('#baffc9').setDescription('Pog you got it right!\n\n(rn you don\'t get money for this yet)')], components: [] });
+    else interaction.reply({ embeds: [new MessageEmbed().setColor('#ff7784').setDescription('You got it wrong :(\n\n(rn you don\'t get money for this yet)')], components: [] });
   }
 };
