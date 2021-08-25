be7351871cb51a12a80ed49d609aea67e78059b3
CactusKing101 <chalkking17@gmail.com>
2021-08-24 22:39:15 -0400

    Update flag.js

--- commands/flag.js
+++ commands/flag.js
@@ -11,9 +11,11 @@ module.exports = {
   executeI(client, interaction) {
     var random = Math.floor(flags.length * Math.random());
     var options = [{ label: flags[random][1], value: `${flags[random][0]}-${flags[random][0]}` }];
+    var randoms = [random];
     for(let i = 0; i < 4; ++i) {
       var random2 = Math.floor(flags.length * Math.random());
-      if (options.includes({ label: flags[random2][1],value: `${flags[random2][0]}-${flags[random][0]}`})) continue;
+      if (randoms.includes(random2)) continue;
+      randoms.push(random2);
       options.push({ label: flags[random2][1], value: `${flags[random2][0]}-${flags[random][0]}` });
     }
     const row = new MessageActionRow()
