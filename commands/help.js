8a2aa1cff93e664d63acf659dde463b9c43923fb
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:14:06 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,8 +4,7 @@ module.exports = {
 	name: 'help',
 	description: 'Sends the current commands',
   usage: `help [command]`,
-  command: true,
-  aliases: ['help'],
+  command: false,
   slash: true,
   options: [
     {
@@ -29,20 +28,5 @@ module.exports = {
       embed.setTitle('Commands:').setDescription(description);
     }
     interaction.reply({ embeds : [embed] });
-  },
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands) {
-    var embed = new MessageEmbed().setColor('#9e9d9d')
-    if (commands.has(args[0]) && commands.get(args[0].toLowerCase()).command) {
-      embed.setTitle(commands.get(args[0].toLowerCase()).name)
-        .setDescription(`Description:\`\`\`\n${commands.get(args[0].toLowerCase()).description}\n\`\`\`Aliases:\`\`\`\n${commands.get(args[0].toLowerCase()).aliases}\`\`\`Usage:\`\`\`\n${prefix}${commands.get(args[0].toLowerCase()).usage}\n\`\`\``);
-    } else {
-      let description = '\`\`\`';
-      commands.forEach((value, key) => {
-        if (value.command) description += `${value.name} - ${value.description}\n`;
-      });
-      description += `\`\`\``;
-      embed.setTitle('Commands:').setDescription(description);
-    }
-    msg.channel.send({ embeds : [embed] });
-	},
+  }
 };
