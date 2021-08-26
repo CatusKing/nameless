52826dc0a1f951d250f8547ebf0e31903a19763a
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:13:35 -0400

    Update admins.js

--- commands/admins.js
+++ commands/admins.js
@@ -4,8 +4,7 @@ module.exports = {
 	name: 'admins',
 	description: 'Displays the current members with admin mode',
   usage: `admins`,
-  command: true,
-  aliases: ['admins'],
+  command: false,
   slash: true,
   options: [],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
@@ -14,12 +13,5 @@ module.exports = {
       description += `${client.users.cache.get(i).tag} - ${i}\n`
     }
     interaction.reply({ embeds: [ new MessageEmbed().setDescription(description).setColor('#9e9d9d') ] });
-  },
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins) {
-    var description = 'Admins\n';
-    for (let i of admins) {
-      description += `${client.users.cache.get(i).tag} - ${i}\n`
-    }
-    reply(msg.channel.id, description, '#9e9d9d');
-	},
+  }
 };
