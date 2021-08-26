120bebe03cb502927ef5fbc0cbe54b3a0f560602
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:13:31 -0400

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -4,8 +4,7 @@ module.exports = {
 	name: 'admin',
 	description: 'Toggles admin mode',
   usage: `admin`,
-  command: true,
-  aliases: ['admin'],
+  command: false,
   slash: true,
   options: [],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
@@ -31,29 +30,5 @@ module.exports = {
       }
     });
     if (yes) interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Sorry you don't have perms for this`).setColor('#9e9d9d') ] });
-  },
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins) {
-    var yes = true;
-    msg.member.roles.cache.forEach(r => {
-      if (adminRoles.includes(r.id) && yes) {
-
-        if (admins.includes(msg.author.id)) {
-          for (var i = 0; i < admins.length; i++) {
-  
-            if (admins[i] == msg.author.id) {
-              admins.splice(i, 1);
-              reply(msg.channel.id, `No longer ignoring you from auto mod\nid: ${msg.author.id}`, '#9e9d9d');
-              break;
-            }
-          }
-        } else {
-          admins.push(msg.author.id);
-          reply(msg.channel.id, `Ignoring you from auto mod\nid: ${msg.author.id}`, '#9e9d9d');
-        }
-        setServerAdmins(admins);
-        yes = false;
-      }
-    });
-    if (yes) reply(msg.channel.id, `Sorry you don't have perms for this`, '#9e9d9d');
-  },
+  }
 };
