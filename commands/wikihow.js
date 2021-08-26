baf77477c9543558ddf3f3a9acea617f77f21abb
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:14:25 -0400

    Update wikihow.js

--- commands/wikihow.js
+++ commands/wikihow.js
@@ -5,8 +5,7 @@ module.exports = {
   name: 'wikihow',
   description: 'Retrieves a random WikiHow article',
   usage: `wikihow`,
-  command: true,
-  aliases: ['wikihow', 'wh'],
+  command: false,
   slash: true,
   options: [],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
@@ -25,25 +24,6 @@ module.exports = {
         interaction.reply(`idk dude the code is scuffed(error)`);
       } else {
         interaction.reply({ embeds: [embed] });
-      }
-    });
-  },
-  execute(client, msg, args, reply) {
-    request('http://www.wikihow.com/Special:Randomizer', { json: false }, (err, res, body) => {
-      if (err) console.warn(err);
-      const dom = new jsdom.JSDOM(body);
-      var yes = true;
-      const embed = new MessageEmbed().setDescription(`[link](https://www.wikihow.com${res.socket._httpMessage.path})\n${dom.window.document.querySelector("title").textContent}`).setColor('#9e9d9d');
-      dom.window.document.querySelectorAll('img').forEach((value) => {
-        if (value.src.includes('https://') && value.src.includes(dom.window.document.querySelector("title").textContent.split(' ')[0]) && yes) {
-          yes = false;
-          embed.setImage(value.src).setDescription(`${embed.description}\n[image](${value.src})`);
-        }
-      });
-      if (embed.description == null) {
-        reply(msg.channel.id, `idk dude the code is scuffed(error)`);
-      } else {
-        msg.channel.send({ embeds: [embed] });
       }
     });
   }
