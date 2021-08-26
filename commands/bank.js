f1932c756be11013de76bf59e3654f91a5805d87
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:13:39 -0400

    Update bank.js

--- commands/bank.js
+++ commands/bank.js
@@ -4,15 +4,11 @@ module.exports = {
 	name: 'bank',
 	description: 'Display\'s the bank\'s balance',
 	usage: `bank`,
-	command: true,
-	aliases: ['bank', 'b'],
+	command: false,
 	slash: true,
 	options: [],
 	executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor) {
 		var embed = new MessageEmbed().setDescription(`The bank currently has ${floor(getUserBalance('bank'))}🦴(${getUserBalance('bank')}🦴)`).setColor('#ffffba');
     interaction.reply({ embeds: [embed] });
-  },
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor) {
-    reply(msg.channel.id, `The bank currently has ${floor(getUserBalance('bank'))}🦴(${getUserBalance('bank')}🦴)`, '#ffffba')
-	}
+  }
 };
