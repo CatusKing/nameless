fc59870fdcc711df125f76a9c72959e249dac589
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:13:56 -0400

    Update catfact.js

--- commands/catfact.js
+++ commands/catfact.js
@@ -5,18 +5,12 @@ module.exports = {
 	name: 'catfact',
 	description: 'Sends a random cat fact',
 	usage: `catfact`,
-	command: true,
-	aliases: ['catfact'],
+	command: false,
   slash: true,
   options: [],
   executeI(client, interaction) {
     request('https://meowfacts.herokuapp.com/', { json: true }, (err, res, body) => {
       interaction.reply({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setDescription(body.data[0]) ] });
-    });
-  },
-	execute(client, msg) {
-    request('https://meowfacts.herokuapp.com/', { json: true }, (err, res, body) => {
-      msg.channel.send({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setDescription(body.data[0]) ] });
     });
   }
 };
