ee4d0d7dc42ab492cf483c6c6020c2a865268bf1
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:13:36 -0400

    Update advice.js

--- commands/advice.js
+++ commands/advice.js
@@ -4,20 +4,13 @@ module.exports = {
 	name: 'advice',
 	description: 'Gives funky advice',
   usage: `advice`,
-  command: true,
-  aliases: ['advice'],
+  command: false,
   slash: true,
   options: [],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     request(`https://api.adviceslip.com/advice`, { json: true }, (err, res, body) => {
       if (err) return console.log(err);
       interaction.reply({ embeds: [ new MessageEmbed().setDescription(body.slip.advice).setColor('#9e9d9d') ] });
-    });
-  },
-	execute(client, msg, args, reply) {
-    request(`https://api.adviceslip.com/advice`, { json: true }, (err, res, body) => {
-      if (err) return console.log(err);
-      reply(msg.channel.id, body.slip.advice, '#9e9d9d');
     });
   }
 };
