c16011d908bd4e53e338884b771f1f3801eedc6e
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:13:29 -0400

    yes

--- commands/add.js
+++ commands/add.js
@@ -4,8 +4,7 @@ module.exports = {
 	name: 'add',
 	description: 'Adds to the users balance',
   usage: `add <amount> [@User]`,
-  command: true,
-  aliases: ['add'],
+  command: false,
   slash: true,
   options: [
     {
@@ -30,17 +29,5 @@ module.exports = {
       interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Given ${amount}🦴 to ${target}\nThey now have ${balance}🦴`).setColor('#baffc9') ] });
       log('830503210951245865', `+${amount}🦴 to ${target} given by ${interaction.user}`, '#baffc9');
     } else interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Sorry you don't have perms for this`).setColor('#9e9d9d') ] });
-  },
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance) {
-    if (msg.member.roles.cache.has('830496065366130709')) {
-      const target = msg.mentions.users.first() || msg.author;
-  
-      if (isNaN(args[0])) return reply(msg.channel.id, `Sorry you need to use the command like this ${prefix}add <amount> [@User]`, '#9e9d9d');
-      const amount = Math.floor(args[0]);
-      const balance = addUserBalance(target.id, amount);
-      addUserBalance('bank', -amount);
-      reply(msg.channel.id, `Given ${amount}🦴 to ${target}\nThey now have ${balance}🦴`, '#baffc9');
-      log('830503210951245865', `+${amount}🦴 to ${target} given by ${msg.author}`, '#baffc9');
-    } else return reply(msg.channel.id, `Sorry you don't have perms for this`, '#9e9d9d');
   }
 };
