cac83a1008f0fbed38d62238ccd74e4bf328509e
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:14:24 -0400

    Update weekly.js

--- commands/weekly.js
+++ commands/weekly.js
@@ -4,8 +4,7 @@ module.exports = {
 	name: 'weekly',
 	description: 'Claim your weekly reward',
   usage: `weekly`,
-  command: true,
-  aliases: ['weekly'],
+  command: false,
   slash: true,
   options: [],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
@@ -24,24 +23,6 @@ module.exports = {
       else if (result == 1) result = `${result} hour`;
       else result = `${result} hours`;
       interaction.reply({ embeds: [ new MessageEmbed().setDescription(`${interaction.user} you have already claimed for this week\nYou can claim again in ${result}`).setColor('#9e9d9d') ] });
-    }
-  },
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance) {
-    const weekly = getUserWeekly(msg.author.id);
-
-    if (weekly <= hours(Date.now())) {
-      addUserBalance(msg.author.id, weeklyAmount);
-      addUserBalance('bank', -weeklyAmount);
-      setUserWeekly(msg.author.id, hours(Date.now()) + 167);
-      reply(msg.channel.id, `${msg.author} just claimed ${weeklyAmount}🦴 for the week`, '#baffc9');
-      log('830503210951245865', `+${weeklyAmount}🦴 to ${msg.author} for their weekly claim`, '#baffc9');
-    } else {
-      let result = weekly - hours(Date.now());
-
-      if (result > 24) result = `${Math.floor(result / 24) + 1} days`;
-      else if (result == 1) result = `${result} hour`;
-      else result = `${result} hours`;
-      reply(msg.channel.id, `${msg.author} you have already claimed for this week\nYou can claim again in ${result}`, '#9e9d9d');
     }
   }
 };
