0e1bddbb314ec6912c88745d343e030648a38da5
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:14:08 -0400

    Update ignore.js

--- commands/ignore.js
+++ commands/ignore.js
@@ -4,8 +4,7 @@ module.exports = {
 	name: 'ignore',
 	description: 'Toggles ignore mode for the current channel',
   usage: `ignore`,
-  command: true,
-  aliases: ['ignore'],
+  command: false,
   slash: true,
   options: [],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
@@ -26,24 +25,5 @@ module.exports = {
       setServerIgnoredCh(ignoredCh);
       return ignoredCh;
     } else interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Sorry you don't have perms for this`).setColor('#9e9d9d') ] });
-  },
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh) {
-    if (msg.member.roles.cache.has('830496065366130709')) {
-      if (ignoredCh.includes(msg.channel.id)) {
-        for (var i = 0; i < ignoredCh.length; i++) {
-
-          if (ignoredCh[i] == msg.channel.id) {
-            ignoredCh.splice(i, 1);
-            reply(msg.channel.id, `No longer ignoring this channel\nid: ${msg.channel.id}`, '#9e9d9d');
-            break;
-          }
-        }
-      } else {
-        ignoredCh.push(msg.channel.id);
-        reply(msg.channel.id, `Ignoring this channel from auto mod\nid: ${msg.channel.id}`, '#9e9d9d');
-      }
-      setServerIgnoredCh(ignoredCh);
-      return ignoredCh;
-    } else return reply(msg.channel.id, `Sorry you don't have perms for this`, '#9e9d9d');
-	},
+  }
 };
