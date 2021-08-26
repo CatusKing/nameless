9bd50b3e16688a973fc0cbe837c9e85bb0cefa53
CactusKing101 <chalkking17@gmail.com>
2021-08-26 00:06:42 -0400

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -4,8 +4,7 @@ module.exports = {
 	name: 'unmute',
 	description: 'Unmute another user',
   usage: `unmute [@User]`,
-  command: true,
-  aliases: ['unmute'],
+  command: false,
   slash: true,
   options: [
     {
@@ -24,21 +23,8 @@ module.exports = {
         target.roles.remove(role, `Muted removed by ${interaction.user}`);
         setUserMuted(target.id, 0);
         interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Unmuted ${target}\nAction by ${interaction.user}`).setColor('#9e9d9d') ] });
-        log(msg.channel.id, `Unmuted ${target}\nAction by ${msg.author}`, '#9e9d9d');
+        log(interaction.channel.id, `Unmuted ${target}\nAction by ${interaction.user}`, '#9e9d9d');
       } else interaction.reply({ embeds: [ new MessageEmbed().setDescription('This user isn\'t muted').setColor('#9e9d9d') ] });
     } else interaction.reply({ embeds: [ new MessageEmbed().setDescription(`You don't have perms for this`).setColor('#9e9d9d') ] });
-  },
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted) {
-    if (msg.member.roles.cache.has('830496065366130709') || msg.member.roles.cache.has('830495937301577759') || msg.member.roles.cache.has('830495908336369694')) {
-      const target = msg.mentions.members.first() || msg.member;
-      const muted = getUserMuted(target.id);
-      if (muted != 0) {
-        const role = client.guilds.cache.get('830495072876494879').roles.cache.get('830495536582361128');
-        target.roles.remove(role, `Muted removed by ${msg.author}`);
-        setUserMuted(target.id, 0);
-        reply(msg.channel.id, `Unmuted ${target}\nAction by ${msg.author}`, '#9e9d9d');
-        log(msg.channel.id, `Unmuted ${target}\nAction by ${msg.author}\n[Jump to!](${msg.url})`, '#9e9d9d');
-      } else reply(msg.channel.id, 'This user isn\'t muted', '#9e9d9d');
-    } else reply(msg.channel.id, `You don't have perms for this`, `#9e9d9d`);
   }
 };
