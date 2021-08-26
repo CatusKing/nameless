d0c73f55671c9320ec978b526a19c95c01549f3d
CactusKing101 <chalkking17@gmail.com>
2021-08-26 16:14:15 -0400

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -4,8 +4,7 @@ module.exports = {
 	name: 'leaderboard',
 	description: 'Updates the leaderboard',
   usage: `leaderboard`,
-  command: true,
-  aliases: ['leaderboard', 'lb'],
+  command: false,
   slash: true,
   options: [],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
@@ -13,11 +12,5 @@ module.exports = {
       updateLeaderboard(client, db, round);
       interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Updated the leaderboard`).setColor('#ffffba') ] });
     } else interaction.reply({ embeds: [ new MessageEmbed().setDescription(`You don't have perms for that you dumb`).setColor('#ffffba') ] });
-  },
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
-    if (msg.member.roles.cache.has('830496065366130709')) {
-      updateLeaderboard(client, db, round);
-      reply(msg.channel.id, `Updated the leaderboard`, '#ffffba');
-    } else reply(msg.channel.id, `You don't have perms for that you dumb`, '#9e9d9d');
   }
 };
