1aae8512c27c2c0fefb1107350e0d58c978a84db
CactusKing101 <chalkking17@gmail.com>
2021-08-27 02:04:09 -0400

    Update hue.js

--- commands/hue.js
+++ commands/hue.js
@@ -8,26 +8,20 @@ module.exports = {
   slash: true,
   options: [
     {
-      name: 'on',
-      description: 'Turns on the set lights',
-      type: 'SUB_COMMAND_GROUP',
-    },
-    {
       name: 'bulb',
       description: 'The bulb(s) you want to turn on',
       type: 'STRING',
       required: true
     },
     {
-      name: 'off',
-      description: 'Turns off the set lights',
+      name: 'on',
+      description: 'Turns on the set lights',
       type: 'SUB_COMMAND_GROUP',
     },
     {
-      name: 'bulb',
-      description: 'The bulb(s) you want to turn off',
-      type: 'STRING',
-      required: true
+      name: 'off',
+      description: 'Turns off the set lights',
+      type: 'SUB_COMMAND_GROUP',
     },
   ],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
