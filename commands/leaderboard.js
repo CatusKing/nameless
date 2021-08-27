db2dfe65d9d1689a032dceee262d6fa0704afab5
CactusKing101 <chalkking17@gmail.com>
2021-08-27 15:42:18 -0400

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -8,9 +8,26 @@ module.exports = {
   slash: true,
   options: [],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
-    if (interaction.member.roles.cache.has('830496065366130709')) {
-      updateLeaderboard(client, db, round);
-      interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Updated the leaderboard`).setColor('#ffffba') ] });
-    } else interaction.reply({ embeds: [ new MessageEmbed().setDescription(`You don't have perms for that you dumb`).setColor('#ffffba') ] });
+    const guildMembers = client.guilds.cache.get('830495072876494879').members.cache;
+    const lb = db.get(`discord.server.leaderboard`) || [];
+    const leaderboard = new Collection();
+    for (let i = 0; i < lb.length; ++i) {
+      if (guildMembers.has(lb[i][0])) {
+        leaderboard.set(lb[i][0], lb[i][1]);
+      }
+    }
+    let description = '```';
+    let num = 1;
+    leaderboard.sort((a, b) => b - a)
+      .filter((value, key) => client.users.cache.has(key))
+      .forEach((value, key) => {
+        if (num <= leaderboard_count) {
+          description += `\n< ${num} > ${round(value)}🦴 ${client.users.cache.get(key).tag}`;
+        }
+        ++num;
+      });
+    description += '```';
+    var embed = new MessageEmbed().setColor('#ffffba').setDescription(description);
+    interaction.reply({ embeds: [embed] });
   }
 };
