7f8909c9fd17163e8e4ca3c6da46ab271d116841
CactusKing101 <chalkking17@gmail.com>
2021-09-01 19:56:35 -0400

    Update ghibli.js

--- commands/ghibli.js
+++ commands/ghibli.js
@@ -9,13 +9,15 @@ module.exports = {
   slash: true,
   options: [],
   executeI(client, interaction) {
+    if (interaction.user.id != '473110112844644372') return
     request('https://ghibliapi.herokuapp.com/films', { json: true }, (err, res, body) => {
-      interaction.reply({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setImage(`https://cataas.com${body.url}`) ], components: [new MessageActionRow().addComponents(new MessageButton().setCustomId('films_1').setLabel('Next').setStyle('PRIMARY'))] });
+      interaction.reply({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setTitle(body[0].title).setDescription(body[0].description).setAuthor(body[0].director) ], components: [new MessageActionRow().addComponents(new MessageButton().setCustomId('films_1').setLabel('Next').setStyle('PRIMARY'))] });
     });
   },
   button: true,
   buttonId: '_',
   executeB(client, interaction) {
+    if (interaction.user.id != '473110112844644372') return
     interaction.reply('buuttooon')
   }
 };
