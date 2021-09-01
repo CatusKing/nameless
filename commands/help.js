b05d9b8c9f85a0e80800aebb3dc508c13718adcd
CactusKing101 <chalkking17@gmail.com>
2021-09-01 17:47:23 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -35,8 +35,9 @@ module.exports = {
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     var embed = new MessageEmbed().setColor('#9e9d9d')
     if ((commands.has(interaction.options.getString('command')) && commands.get(interaction.options.getString('command').toLowerCase()).command) || (commands.has(interaction.options.getString('moreCommands')) && commands.get(interaction.options.getString('moreCommands').toLowerCase()).command)) {
-      embed.setTitle(commands.get(interaction.options.getString('command').toLowerCase()).name)
-        .setDescription(`Description:\`\`\`\n${commands.get(interaction.options.getString('command').toLowerCase()).description}\n\`\`\`Aliases:\`\`\`\n${commands.get(interaction.options.getString('command').toLowerCase()).aliases}\`\`\`Usage:\`\`\`\n${prefix}${commands.get(interaction.options.getString('command').toLowerCase()).usage}\n\`\`\``);
+      var command = interaction.options.getString('command') || interaction.options.getString('moreCommands');
+      embed.setTitle(commands.get(command).name)
+        .setDescription(`Description:\`\`\`\n${commands.get(command).description}\n\`\`\`Aliases:\`\`\`\n${commands.get(command).aliases}\`\`\`Usage:\`\`\`\n${prefix}${commands.get(command).usage}\n\`\`\``);
     } else {
       let description = '\`\`\`';
       commands.forEach((value, key) => {
