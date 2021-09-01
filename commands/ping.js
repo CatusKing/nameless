0aeb6b39970bd3b75e1f3838ff495a2941b65c9a
CactusKing101 <chalkking17@gmail.com>
2021-09-01 17:26:09 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,18 @@ module.exports = {
   usage: `ping`,
   command: true,
   aliases: ['ping'],
+  slash: true,
+  executeI(client, interaction) {
+    interaction.reply('Pinging...');
+    interaction.fetchReply().then(reply => {
+      reply.edit('\u200B');
+      const ping = new MessageEmbed()
+        .setColor('#9e9d9d')
+        .setTitle('Pong!')
+        .setDescription(`Roundtrip latency is ${Math.floor(reply.createdTimestamp - interaction.createdTimestamp)}ms \nAPI Latency is ${Math.round(client.ws.ping)}ms`);
+      reply.edit({ embeds: [ping] });
+    });
+  },
 	execute(client, msg) {
     msg.channel.send('Pinging...').then((message) => {
       message.edit("\u200B");
