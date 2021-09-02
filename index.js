103c54e9346506620c533a6b375f1be98f29490e
CactusKing101 <chalkking17@gmail.com>
2021-09-02 19:26:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,10 +41,7 @@ for (const file of functionFiles) {
 math.import({
   'import':     function () { throw new Error('Function import is disabled') },
   'createUnit': function () { throw new Error('Function createUnit is disabled') },
-  'evaluate':   function () { throw new Error('Function evaluate is disabled') },
   'parse':      function () { throw new Error('Function parse is disabled') },
-  'simplify':   function () { throw new Error('Function simplify is disabled') },
-  'derivative': function () { throw new Error('Function derivative is disabled') }
 }, { override: true })
 
 const log = (channelId = new String, content = new String, color = new String) => {
