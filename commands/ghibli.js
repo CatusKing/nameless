3aef196253de3cb18dfdc610f336911c0f0bafa4
CactusKing101 <chalkking17@gmail.com>
2021-09-01 20:12:26 -0400

    Update ghibli.js

--- commands/ghibli.js
+++ commands/ghibli.js
@@ -19,10 +19,9 @@ module.exports = {
   executeB(client, interaction) {
     if (interaction.user.id != '473110112844644372') return;
     var split = interaction.customId.split('_');
+    split[1] = Number(split[1]);
     if (split[0] == 'films') {
       request('https://ghibliapi.herokuapp.com/films', { json: true }, (err, res, body) => {
-        console.log(split)
-        console.log(body.length)
         if (split[1] + 1 > body.length) interaction.update({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setTitle(body[split[1]].title).setDescription(body[split[1]].description).setAuthor(body[split[1]].director) ], components: [new MessageActionRow().addComponents(new MessageButton().setCustomId(`films_${split[1] - 1}`).setLabel('Back').setStyle('PRIMARY'))] });
         else if (split[1] == 0) interaction.update({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setTitle(body[0].title).setDescription(body[0].description).setAuthor(body[0].director) ], components: [new MessageActionRow().addComponents(new MessageButton().setCustomId('films_1').setLabel('Next').setStyle('PRIMARY'))] });
         else interaction.update({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setTitle(body[split[1]].title).setDescription(body[split[1]].description).setAuthor(body[split[1]].director) ], components: [new MessageActionRow().addComponents([new MessageButton().setCustomId(`films_${split[1] - 1}`).setLabel('Back').setStyle('PRIMARY'), new MessageButton().setCustomId(`films_${split[1] + 1}`).setLabel('Next').setStyle('PRIMARY')])] });
