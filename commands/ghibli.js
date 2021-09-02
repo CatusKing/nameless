b4ed4b9f6748004c5218b1c77d47b27de74b68da
CactusKing101 <chalkking17@gmail.com>
2021-09-01 20:42:10 -0400

    Update ghibli.js

--- commands/ghibli.js
+++ commands/ghibli.js
@@ -21,18 +21,27 @@ module.exports = {
     var split = interaction.customId.split('_');
     split[1] = Number(split[1]);
     if (split[0] == 'films') {
-      if (split[2] == 'people') {
-
-      } else {
-        request('https://ghibliapi.herokuapp.com/films', { json: true }, (err, res, body) => {
-          var components = [];
-          if (split[1] + 1 >= body.length) components.push(new MessageActionRow().addComponents(new MessageButton().setCustomId(`films_${split[1] - 1}`).setLabel('Back').setStyle('PRIMARY')));
-          else if (split[1] == 0) components.push(new MessageActionRow().addComponents(new MessageButton().setCustomId('films_1').setLabel('Next').setStyle('PRIMARY')));
-          else components.push(new MessageActionRow().addComponents([new MessageButton().setCustomId(`films_${split[1] - 1}`).setLabel('Back').setStyle('PRIMARY'), new MessageButton().setCustomId(`films_${split[1] + 1}`).setLabel('Next').setStyle('PRIMARY')]));
+      request('https://ghibliapi.herokuapp.com/films', { json: true }, (err, res, body) => {
+        var components = [];
+        if (split[1] + 1 >= body.length) components.push(new MessageActionRow().addComponents(new MessageButton().setCustomId(`films_${split[1] - 1}`).setLabel('Back').setStyle('PRIMARY')));
+        else if (split[1] == 0) components.push(new MessageActionRow().addComponents(new MessageButton().setCustomId('films_1').setLabel('Next').setStyle('PRIMARY')));
+        else components.push(new MessageActionRow().addComponents([new MessageButton().setCustomId(`films_${split[1] - 1}`).setLabel('Back').setStyle('PRIMARY'), new MessageButton().setCustomId(`films_${split[1] + 1}`).setLabel('Next').setStyle('PRIMARY')]));
+        if (split[2] == 'people') {
+          components.push(new MessageActionRow().addComponents(new MessageButton().setCustomId(`films_${split[1]}`).setLabel('Back to Film').setStyle('SECONDARY')));
+          if (body[split[1]].people[0] == 'https://ghibliapi.herokuapp.com/people/') {
+            interaction.update({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setTitle(body[split[1]].title).setDescription(`The people for this film are not set :(`).setAuthor(`Director: ${body[split[1]].director}`).setFooter(`${split[1] + 1}/${body.length}`) ], components: components });  
+          } else {
+            if (split[3] == null) {
+              request(body[split[1]].people[0], { json: true }, (err, res, body2) => {
+                interaction.update({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setTitle(body2.name).setDescription(`Age: \`${body.age}\`\nGender: \`${body2.gender}\`\nHair Color: \`${body.hair_color}\`\nEye Color: \`${body2.eye_color}\``).setAuthor(`Director: ${body[split[1]].director}`).setFooter(`${split[1] + 1}/${body.length}`) ], components: components });  
+              });
+            }
+          }
+        } else {
           components.push(new MessageActionRow().addComponents(new MessageButton().setCustomId(`films_${split[1]}_people`).setLabel('People').setStyle('SECONDARY')));
-          interaction.update({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setTitle(body[split[1]].title).setDescription(body[split[1]].description).setAuthor(`Director: ${body[split[1]].director}`).setFooter(`${split[1] + 1}/${body.length}`) ], components: components });
-        });
-      }
+          interaction.update({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setTitle(body[split[1]].title).setDescription(body[split[1]].description).setAuthor(`Director: ${body[split[1]].director}`).setFooter(`${split[1] + 1}/${body.length}`) ], components: components });  
+        }
+      });
     }
   }
 };
