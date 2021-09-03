a01e28724765523fb5a359ca8c365ac9da7f4cd8
CactusKing101 <chalkking17@gmail.com>
2021-09-02 20:29:58 -0400

    Update flag.js

--- commands/flag.js
+++ commands/flag.js
@@ -34,7 +34,7 @@ module.exports = {
       components.push(new MessageButton()
 				.setCustomId(options[i].value)
 				.setLabel(options[i].label)
-				.setStyle('SECONDARY'),
+				.setStyle('DANGER'),
 			);
     }
     interaction.reply({ 
