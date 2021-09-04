bc5bf95ada8d3da1da2aba41b4339b6d51259f83
CactusKing101 <chalkking17@gmail.com>
2021-09-04 01:10:55 -0400

    Create streak.js

---
+++ commands/streak.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'streak',
+	description: 'Gives you your current streak',
+  usage: `streak`,
+  command: false,
+  slash: true,
+  options: [],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    var streak = db.get(`discord.users.${interaction.user.id}.streak`) || 0
+    interaction.reply(streak)
+  }
+};
