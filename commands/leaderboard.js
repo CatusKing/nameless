02141d5ce65d5b0e93c6ff0a72e88cca5ff3c198
CactusKing101 <chalkking17@gmail.com>
2021-09-04 19:27:55 -0400

    yep

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,4 +1,5 @@
 const { MessageEmbed, Collection } = require("discord.js");
+const { leaderboard_count } = require("../general/config.json");
 
 module.exports = {
 	name: 'leaderboard',
@@ -11,30 +12,58 @@ module.exports = {
       type: 'SUB_COMMAND',
       name: 'streaks',
       description: 'Gives you the Streak Leaderboard',
-      required: false
+    },
+    {
+      type: 'SUB_COMMAND',
+      name: 'balance',
+      description: 'Gives you the Balance Leaderboard',
     }
   ],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
-    const guildMembers = client.guilds.cache.get('830495072876494879').members.cache;
-    const lb = db.get(`discord.server.leaderboard`) || [];
-    const leaderboard = new Collection();
-    for (let i = 0; i < lb.length; ++i) {
-      if (guildMembers.has(lb[i][0])) {
-        leaderboard.set(lb[i][0], lb[i][1]);
-      }
-    }
-    let description = '```';
-    let num = 1;
-    leaderboard.sort((a, b) => b - a)
-      .filter((value, key) => client.users.cache.has(key))
-      .forEach((value, key) => {
-        if (num <= 10) {
-          description += `\n< ${num} > ${round(value)}🦴 ${client.users.cache.get(key).tag}`;
+    if (interaction.options.getSubcommand() == 'balance') {
+      const guildMembers = client.guilds.cache.get('830495072876494879').members.cache;
+      const lb = db.get(`discord.server.leaderboard`) || [];
+      const leaderboard = new Collection();
+      for (let i = 0; i < lb.length; ++i) {
+        if (guildMembers.has(lb[i][0])) {
+          leaderboard.set(lb[i][0], lb[i][1]);
         }
-        ++num;
+      }
+      let description = '```';
+      let num = 1;
+      leaderboard.sort((a, b) => b - a)
+        .filter((value, key) => client.users.cache.has(key))
+        .forEach((value, key) => {
+          if (num <= leaderboard_count) {
+            description += `\n< ${num} > ${round(value)}🦴 ${client.users.cache.get(key).tag}`;
+          }
+          ++num;
+        });
+      description += '```';
+      var embed = new MessageEmbed().setColor('#9e9d9d').setDescription(description);
+      interaction.reply({ embeds: [embed] });
+    } else {
+      const guildMembers = client.guilds.cache.get('830495072876494879').members.cache;
+      const users = db.get(`discord.users`) || {};
+      const leaderboard = new Collection();
+      guildMembers.forEach((member) => {
+        if (users[member.id]) {
+          leaderboard.set(member.id, users[member.id].streak || 0);
+        }
       });
-    description += '```';
-    var embed = new MessageEmbed().setColor('#ffffba').setDescription(description);
-    interaction.reply({ embeds: [embed] });
+      let description = '```';
+      let num = 1;
+      leaderboard.sort((a, b) => b - a)
+        .filter((value, key) => client.users.cache.has(key))
+        .forEach((value, key) => {
+          if (num <= leaderboard_count) {
+            description += `\n< ${num} > ${value}🔥 ${client.users.cache.get(key).tag}`;
+          }
+          ++num;
+        });
+      description += '```';
+      var embed = new MessageEmbed().setColor('#9e9d9d').setDescription(description);
+      interaction.reply({ embeds: [embed] });
+    }
   }
 };
