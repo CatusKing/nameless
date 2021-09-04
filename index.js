8e1a6245baa1ac43032ff46f8b4cd6b8cec3a146
CactusKing101 <chalkking17@gmail.com>
2021-09-04 13:21:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -484,7 +484,6 @@ const counting = () => {
 };
 
 const checkStreaks = () => {
-  console.debug('e')
   var users = db.get(`discord.users`) || {};
   var date = new Date();
   var guild = client.guilds.cache.get('830495072876494879');
@@ -494,6 +493,15 @@ const checkStreaks = () => {
       var streakTime = users[member.id].streakTime || 0;
       if (streakTime <= Math.floor(((date.getTime() / 1000) / 60) / 60)) {
         users[member.id].streak = 0;
+      } else {
+        for(let i = 0; i < config.streaks; ++i) {
+          const role = guild.roles.cache.get(config.streaks[i][0]);
+          if (streak >= config.streaks[i][0] && !member.roles.cache.has(role.id)) {
+            member.roles.add(role, 'New Streak Role');
+          } else if (member.roles.cache.has(role.id)) {
+            member.roles.remove(role, 'Reset Streak :(');
+          }
+        }
       }
     }
   });
