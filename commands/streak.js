9e46ad7092d3d90f4838fb03ede5fd6610a05d50
CactusKing101 <chalkking17@gmail.com>
2021-09-04 01:13:35 -0400

    Update streak.js

--- commands/streak.js
+++ commands/streak.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
 	name: 'streak',
 	description: 'Gives you your current streak',
@@ -7,6 +9,6 @@ module.exports = {
   options: [],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     var streak = db.get(`discord.users.${interaction.user.id}.streak`) || 0
-    interaction.reply({ content: `${streak}` })
+    interaction.reply({ embeds: [new MessageEmbed().setColor('#9e9d9d').setDescription(`Your current streak is ${streak}`)] })
   }
 };
