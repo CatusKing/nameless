c1edc033b2fbcf6d8f28bd56a94f4824b8c830df
CactusKing101 <chalkking17@gmail.com>
2021-09-06 13:36:09 -0400

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -10,9 +10,11 @@ module.exports = {
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     if (interaction.user.id != '473110112844644372') return interaction.reply(`<@473110112844644372>`);
     interaction.reply({ embeds: [ new MessageEmbed().setDescription('Restarting...').setColor('#9e9d9d') ] });
-    console.log('Closing all connections...');
-    client.destroy();
-    console.log('Finished closing connections');
-    process.exit(0);
+    setTimeout(() => {
+      console.log('Closing all connections...');
+      client.destroy();
+      console.log('Finished closing connections');
+      process.exit(0);
+    }, 1500)
   }
 };
