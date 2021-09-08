d6f7d0b15d22bc934519a08d59dc815721fbc177
Thomas <chalkking17@gmail.com>
2021-09-08 19:05:09 -0400

    Create pull.js

---
+++ commands/pull.js
@@ -0,0 +1,15 @@
+const { exec } = require("child_process");
+
+module.exports = {
+	name: 'pull',
+	description: 'Pulls the latest code from the repo',
+  command: false,
+  slash: true,
+  options: [],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    if (interaction.user.id == '473110112844644372') {
+      exec('git pull')
+      interaction.reply('pog i think it worked');
+    } else interaction.reply('no');
+  }
+};
