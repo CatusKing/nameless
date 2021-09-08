3e05b3dbbe2643806d4dd46600dbb4b72d8705cb
CactusKing101 <chalkking17@gmail.com>
2021-09-08 14:36:35 -0400

    Create streaks.js

---
+++ commands/streaks.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require("discord.js");
+const { streaks } = require('../general/config.json');
+
+module.exports = {
+  name: 'streaks',
+  description: 'Lists all the possible streak rewards',
+  command: false,
+  slash: true,
+  options: [],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    var description = '';
+    streaks.forEach(value => {
+      description += `${floor(value[2])}🦴 - <@&${value[1]}>\n`;
+    });
+    interaction.reply({ embeds: [new MessageEmbed().setDescription().setColor('#9e9d9d')] })
+  }
+};
