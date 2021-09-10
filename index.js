c47d8ddc82ddcb888627e1a8b87e3fa824af3bdd
CactusKing101 <chalkking17@gmail.com>
2021-09-09 20:55:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -265,7 +265,7 @@ const nextLaunch = () => client.functions.get('nextLaunch').execute(client);
 const events = () => client.functions.get('events').execute();
 
 const counting = () => {
-  count = client.functions.get('counting').execute(client, db, limitedEvaluate, log, addUserBalance, count, topCount);
+  console.log(client.functions.get('counting').execute(client, db, limitedEvaluate, log, addUserBalance, count, topCount))
   if (count > topCount) {
     topCount = count;
   }
