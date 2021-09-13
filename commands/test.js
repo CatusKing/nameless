0f77156a8a781e74944c97d2173155fd507f7ebb
CactusKing101 <chalkking17@gmail.com>
2021-09-13 19:52:48 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -7,7 +7,9 @@ module.exports = {
   options: [],
   executeI(client = new Client(), interaction = new CommandInteraction()) {
     if (interaction.member.roles.cache.has('830496065366130709')) {
-      console.log(client.guilds.cache.get('887122531050479666').channels.cache)
+      client.guilds.cache.get('887122531050479666').invites.create('887122531050479669').then((invite) => {
+        interaction.reply(invite.code);
+      });
     } else interaction.reply('No perms');
   }
 }
