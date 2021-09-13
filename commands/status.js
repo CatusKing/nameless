8c940f402f1c71e7cf37300761c6b1b9678da050
Thomas <chalkking17@gmail.com>
2021-09-13 08:48:01 -0400

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,12 +1,15 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
 	name: 'status',
 	description: 'Updates the bots status',
-  command: true,
-  aliases: ['status'],
-	execute(client, msg, args, reply, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus) {
-    if (msg.member.roles.cache.has('830496065366130709')) {
+  command: false,
+  slash: true,
+  options: [],
+	executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    if (interaction.member.roles.cache.has('830496065366130709')) {
       updateStatus();
-      reply(msg.channel.id, `Updated the status`, `#9e9d9d`);
-    } else return reply(msg.channel.id, `Sorry you don't have perms for this`, '#9e9d9d');
+      interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Updated the status`).setColor(`#9e9d9d`) ] });
+    } else return interaction.reply({ embeds: [ new MessageEmbed().setDescription(`Sorry you don't have perms for this`).setColor(`#9e9d9d`) ] });
   }
 };
