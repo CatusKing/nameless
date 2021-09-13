b7d47e930f09cc9ffd36cf2452f851e05365202e
CactusKing101 <chalkking17@gmail.com>
2021-09-13 19:46:25 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,7 @@ const { CommandInteraction, Client } = require("discord.js");
 module.exports = {
   name: 'test',
   description: 'Just a simple test command',
-  command: false,
+  slash: true,
   options: [],
   executeI(client = new Client(), interaction = new CommandInteraction()) {
     if (interaction.member.roles.cache.has('')) {
