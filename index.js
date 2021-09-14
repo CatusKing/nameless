c8b5ec7ed6e32d438cf938bcc460a00d23e57a7b
Thomas <chalkking17@gmail.com>
2021-09-14 08:32:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -223,7 +223,6 @@ const counting = () => {
   const channel = client.channels.cache.get('830661661991632907');
   const role = client.guilds.cache.get('830495072876494879').roles.cache.get('830904166007701504');
   channel.messages.fetch({ limit: 10 }, { force: true }).then((messages) => {
-    console.log(messages.first().interaction)
     if (messages.first().interaction) {
       db.set(`discord.count`, 0);
       count = 0;
@@ -232,7 +231,7 @@ const counting = () => {
       client.guilds.cache.get(config.guildId).members.cache.get(messages.first().interaction.user.id).roles.add(role);
       error = true;
     }
-    if (messages.first().bot) return;
+    if (messages.first().author.bot) return;
     var error = false;
     try { var number = limitedEvaluate(messages.first().content.toLowerCase()); }
     catch (err) {
