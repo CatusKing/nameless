e9ce91c9093dab834549ba0ee79f3a9f82a8b734
Thomas <chalkking17@gmail.com>
2021-09-17 09:27:43 -0400

    Update 2048.js

--- commands/2048.js
+++ commands/2048.js
@@ -7,6 +7,6 @@ module.exports = {
   slash: true,
   options: [],
   executeI(client = new Client(), interaction = new CommandInteraction()) {
-    interaction.reply(game.play().prevField)  
+    interaction.reply(game.play().prevField())  
   }
 }
