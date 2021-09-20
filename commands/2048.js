f0a15e4597690224a128309e2e8ded31c4adff7f
Thomas <chalkking17@gmail.com>
2021-09-20 09:27:30 -0400

    Update 2048.js

--- commands/2048.js
+++ commands/2048.js
@@ -12,9 +12,13 @@ module.exports = {
     for(let i = 0; i < table.length; ++i) {
       components[i] = new MessageActionRow()
       for(let j = 0; j < table[i].length; ++j) {
-        components[i].addComponents(new MessageButton().setLabel(`${table[i][j]}`).setStyle('SECONDARY').setCustomId(`${i}-${j}`))
+        components[i].addComponents(new MessageButton().setLabel(`${table[i][j]}`).setStyle('SUCCESS').setCustomId(`${i}-${j}`))
       }
     }
+    components[0].addComponents(new MessageButton().setEmoji('⬆️').customId(`up`));
+    components[3].addComponents(new MessageButton().setEmoji('⬇️').customId(`down`));
+    components[4] = new MessageActionRow()
+    components[4].addComponents([ new MessageButton().setEmoji('⬅️').setCustomId('left'), new MessageButton().setLabel('ye').setCustomId('fill1'), new MessageButton().setLabel('ye').setCustomId('label2'), new MessageButton().setEmoji('➡️').setCustomId('right') ])
     interaction.reply({ content: 'hi', components: components })
   }
 }
