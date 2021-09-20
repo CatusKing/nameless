6cef78ba431beed30055e2c306aa08381dc61bfe
Thomas <chalkking17@gmail.com>
2021-09-20 12:55:19 -0400

    Update 2048.js

--- commands/2048.js
+++ commands/2048.js
@@ -29,7 +29,7 @@ module.exports = {
     if (!preTable) {
       interaction.message.edit({ content: 'No active thingy', components: [] })
     } else {
-      var table = game.play({ prevField: preTable, direction: interaction.component.customId.replace('!', '') });
+      var table = game.play({ prevField: preTable, direction: interaction.component.customId.replace('!', '').toUpperCase() });
       console.log(table)
       var components = [];
       for(let i = 0; i < table.length; ++i) {
