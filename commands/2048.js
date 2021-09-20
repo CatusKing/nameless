43835e1f05e82dfeec4f8c2a6bfe3a730a323b0d
Thomas <chalkking17@gmail.com>
2021-09-20 09:17:04 -0400

    Update 2048.js

--- commands/2048.js
+++ commands/2048.js
@@ -1,4 +1,4 @@
-const { CommandInteraction, Client } = require("discord.js");
+const { CommandInteraction, Client, MessageActionRow, MessageButton } = require("discord.js");
 const game = require('2048_functional');
 
 module.exports = {
@@ -7,7 +7,14 @@ module.exports = {
   slash: true,
   options: [],
   executeI(client = new Client(), interaction = new CommandInteraction()) {
-    var yep = game.play({ isNewGame: true }).nextField;
-    interaction.reply(yep[0].toString())
+    var table = game.play({ isNewGame: true }).nextField;
+    var components = [];
+    for(let i = 0; i < table.length; ++i) {
+      components[i] = new MessageActionRow()
+      for(let j = 0; j < table[i].length; ++j) {
+        components[i].addComponents(new MessageButton().setLabel(table[i][j]).setStyle('SECONDARY').setCustomId(`${i}-${j}`))
+      }
+    }
+    interaction.reply({ content: 'hi', components:  })
   }
 }
