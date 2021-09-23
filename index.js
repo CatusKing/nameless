c38e9b3a26c50a675abc4be63f381b812579f19d
Thomas <chalkking17@gmail.com>
2021-09-23 11:36:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -522,7 +522,7 @@ client.on('interactionCreate', async interaction => {
     client.commands.forEach((value, key) => {
       if (value.name == interaction.commandName && value.slash) {
         try {
-          value.executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, client.commands, client.functions.get('updateLeaderboard').execute, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db/*longest is income*/);
+          value.executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, client.commands, client.functions.get('updateLeaderboard').execute, getUserMuted, setUserMuted, client.functions.get('updateStatus').execute, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db/*longest is income*/);
         } catch (err) {
           interaction.reply('there was an error trying to execute that command!');
           console.error(err);
