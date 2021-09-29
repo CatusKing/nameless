bbca343063b0ccb95dbd370e595b9a031ba1b81f
Thomas <chalkking17@gmail.com>
2021-09-29 14:22:33 -0400

    Update dadjoke.js

--- commands/dadjoke.js
+++ commands/dadjoke.js
@@ -3,7 +3,7 @@ const { icoe } = require('../icoe.js');
 const { get } = require('request');
 
 module.exports = {
-  name: 'dadjokes',
+  name: 'dadjoke',
   description: 'Sends a random dad joke :D',
   slash: true,
   options: [],
@@ -12,7 +12,7 @@ module.exports = {
     if (interaction.options.getString('country')) country = `&country=${interaction.options.getString('country')}`;
     get(`https://icanhazdadjoke.com/`, { json: true, headers: { 'Accept': 'application/json' } }, (err, res, body) => {
       if (err || body.status != 200) return icoe(err);
-      interaction.reply({ embeds: [ new MessageEmbed().setDescription(body.joke).setFooter(body.id).setColor('#9e9d9d') ] })
+      interaction.reply({ embeds: [ new MessageEmbed().setDescription(body.joke).setColor('#9e9d9d') ] })
     });
   },
 }
