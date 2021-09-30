7d197059632240125b467f570fc4e61c89a1b0fb
Thomas <chalkking17@gmail.com>
2021-09-30 16:15:23 -0400

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -1,15 +1,24 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, CommandInteraction } = require("discord.js");
 const request = require("request");
+const { icoe } = require('../icoe');
 
 module.exports = {
   name: 'dog',
   description: 'Sends a random dog image',
   command: false,
   slash: true,
-  options: [],
-  executeI(client, interaction) {
-    request('https://dog.ceo/api/breeds/image/random', { json: true }, (err, res, body) => {
-      interaction.reply({ embeds: [ new MessageEmbed().setColor('#9e9d9d').setImage(body.message) ] });
+  options: [
+    {
+      name: 'breed',
+      type: 'STRING',
+      description: 'The breed being searched for',
+      required: false
+    }
+  ],
+  executeI(client, interaction = new CommandInteraction()) {
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${interaction.options.getString('breed')}`, { json: true }, (err, res, body) => {
+      if (err) return icoe(err);
+      interaction.reply({ embeds: [ new MessageEmbed().setTitle(body[0].name).setFooter(body[0].id).setColor('#9e9d9d') ] })
     });
   }
 };
