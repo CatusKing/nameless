a28b1ba82eaaf54cb7a348f259bb618da10f17b8
Thomas <chalkking17@gmail.com>
2021-09-30 16:26:39 -0400

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -18,7 +18,7 @@ module.exports = {
   executeI(client, interaction = new CommandInteraction()) {
     request(`https://api.thecatapi.com/v1/breeds/search?q=${interaction.options.getString('breed')}`, { json: true }, (err, res, body) => {
       if (err) return icoe(err);
-      interaction.reply({ embeds: [ new MessageEmbed().setTitle(body[0].name).setFooter(body[0].id).setColor('#9e9d9d') ] })
+      interaction.reply({ embeds: [ new MessageEmbed().setTitle(body[0].name).setURL(body[0].wikipedia_url).setFooter(body[0].id).setDescription(body[0].description).addField('Life Span', body[0].life_span).setColor('#9e9d9d').setThumbnail(`https://api.thecatapi.com/v1/images/${body[0].reference_image_id}`) ] })
     });
   }
 };
