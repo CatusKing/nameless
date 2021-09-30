bdc5458a5bdf67580da6566ca233939201c299e0
Thomas <chalkking17@gmail.com>
2021-09-30 16:44:40 -0400

    Update dog.js

--- commands/dog.js
+++ commands/dog.js
@@ -16,7 +16,7 @@ module.exports = {
     }
   ],
   executeI(client, interaction = new CommandInteraction()) {
-    request(`https://api.thecatapi.com/v1/breeds/search?q=${interaction.options.getString('breed')}`, { json: true }, (err, res, body) => {
+    request(`https://api.thedogapi.com/v1/breeds/search?q=${interaction.options.getString('breed')}`, { json: true }, (err, res, body) => {
       if (err) return icoe(err);
       interaction.reply({ embeds: [ new MessageEmbed().setTitle(body[0].name).setURL(body[0].wikipedia_url).setFooter(body[0].id).setDescription(body[0].description).addField('Life Span', `${body[0].life_span} years`, true).addField('Energy Level', `${body[0].energy_level} - 5`, true).addField('Intelligence', `${body[0].intelligence} - 5`, true).addField('Shedding Level', `${body[0].shedding_level} - 5`, true).addField('Social Needs', `${body[0].social_needs} - 5`, true).addField('Affection Level', `${body[0].affection_level} - 5`, true).setColor('#9e9d9d').setThumbnail(`https://api.thecatapi.com/v1/images/${body[0].reference_image_id}`) ] })
     });
