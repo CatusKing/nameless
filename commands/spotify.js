2d850360fb74ffd552ce58aeb1fdba5ed94771a1
Thomas <chalkking17@gmail.com>
2021-10-15 07:46:38 -0400

    Update spotify.js

--- commands/spotify.js
+++ commands/spotify.js
@@ -33,7 +33,7 @@ module.exports = {
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     if (interaction.options.getSubcommand() == 'link') {
       db.set(`discord.users.${interaction.user.id}.spotify`, interaction.options.getString('token'));
-      db.set(`discord.users.${interaction.user.id}.spotify`, interaction.options.getString('refresh'));
+      db.set(`discord.users.${interaction.user.id}.refresh`, interaction.options.getString('refresh'));
       interaction.reply({ content: `Set token to ${interaction.options.getString('token')}`, ephemeral: true });  
     } else {
       db.delete(`discord.users.${interaction.user.id}.spotify`);
