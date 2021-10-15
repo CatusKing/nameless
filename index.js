28674301d17c26a6d0df416e31c0544e567923cf
Thomas <chalkking17@gmail.com>
2021-10-14 20:55:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -378,8 +378,8 @@ const checkSpotify = () => {
         .then(function(data) {
           // Output items
           if (data.body && data.body.is_playing && !data.body.device.is_private_session && data.body.item) {
-            client.guilds.cache.get(config.guildId).channels.cache.get('898257575986991136').send({ embeds: [ new MessageEmbed().setTitle(data.body.item.name).setURL(data.body.item.external_urls['spotify']).setImage(data.body.item.album.images[0].url).setColor('#5de17b') ] })
-            console.log();
+            client.guilds.cache.get(config.guildId).channels.cache.get('898257575986991136').send({ embeds: [ new MessageEmbed().setTitle(data.body.item.name).setURL(data.body.item.external_urls['spotify']).setThumbnail(data.body.item.album.images[0].url).setColor('#5de17b') ] })
+            console.log(data.body.item.artist);
           }
         }, function(err) {
           console.log('Something went wrong!', err);
