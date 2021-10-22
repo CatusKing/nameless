df5db6ddff0a4827bfae6b327206d4452d1f1284
Thomas <chalkking17@gmail.com>
2021-10-22 08:07:32 -0400

    Update love.js

--- commands/love.js
+++ commands/love.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 const { get } = require('request');
 const { icoe } = require('../icoe');
+const { apiKey5 } = require('../general/token.json');
 module.exports = {
 	name: 'love',
 	description: 'Calculate the love of two people by name',
@@ -20,7 +21,7 @@ module.exports = {
     }
   ],
   executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
-    get(`https://love-calculator.p.rapidapi.com/getPercentage?sname=${interaction.options.getString('person-2')}&fname=${interaction.options.getString('person-1')}`, { json: true, headers: { 'x-rapidapi-host': 'love-calculator.p.rapidapi.com', 'x-rapidapi-key': 'e82ba320d2msh04d4c7f7d1c847ap1d05bbjsne099d212cd7c'} }, (err, res, body) => {
+    get(`https://love-calculator.p.rapidapi.com/getPercentage?sname=${interaction.options.getString('person-2')}&fname=${interaction.options.getString('person-1')}`, { json: true, headers: { 'x-rapidapi-host': 'love-calculator.p.rapidapi.com', 'x-rapidapi-key': apiKey5} }, (err, res, body) => {
       if (err) return icoe(err);
       console.log(body)
       interaction.reply({ embeds: [ new MessageEmbed().setDescription(body.result).setColor('#FF58CA').setTitle(`${body.fname} + ${body.sname} = ${body.percentage}%`) ] });
