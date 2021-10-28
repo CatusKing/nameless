ff3f305a24dac99711053c42364ae0cb1f220220
CactusKing101 <chalkking17@gmail.com>
2021-10-28 15:31:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -425,9 +425,9 @@ const checkHolidays = () => {
   })
 };
 
-const namelessChatbot = (channel = new TextChannel()) => {
-  request({method: 'GET',url: 'https://random-stuff-api.p.rapidapi.com/ai',qs: {message: 'sure', server: 'main'},headers: {authorization: 'KzDKANeAok5Z','x-rapidapi-host': 'random-stuff-api.p.rapidapi.com','x-rapidapi-key': token.apiKey5, useQueryString: true}}, (err, res, body) => {
-    channel.send({ content: body.response });
+const namelessChatbot = (msg) => {
+  request({method: 'GET',url: 'https://random-stuff-api.p.rapidapi.com/ai',qs: {message: msg.content, server: 'main'}, json: true,headers: {authorization: token.apiKey6,'x-rapidapi-host': 'random-stuff-api.p.rapidapi.com','x-rapidapi-key': token.apiKey5, useQueryString: true}}, (err, res, body) => {
+    msg.channel.send({ content: body.response });
   });
 }
 
@@ -504,7 +504,7 @@ client.once('ready', () => {
 client.on('messageCreate', async (msg) => {
 
   if (msg.channelId == '830661661991632907') counting();
-  if (msg.channelId == '900421544667414558' && !msg.author.bot) namelessChatbot(msg.channel);
+  if (msg.channelId == '900421544667414558' && !msg.author.bot) namelessChatbot(msg);
 
   if (msg.author.bot || msg.webhookId) return;
 
