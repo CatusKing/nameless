ca135dca3924cc97ed0b143633a0b4044768c069
CactusKing101 <chalkking17@gmail.com>
2021-10-28 16:19:36 -0400

    Update bot.js

--- commands/bot.js
+++ commands/bot.js
@@ -6,7 +6,28 @@ module.exports = {
 	description: 'Displays information on the bot',
   command: false,
   slash: true,
-  options: [],
+  options: [
+    {
+      name: 'option',
+      type: 'STRING',
+      description: 'The Information you want',
+      required: true,
+      choices: [
+        {
+          name: 'Apis',
+          value: 'apis'
+        },
+        {
+          name: 'System',
+          value: 'system'
+        },
+        {
+          name: 'General Information',
+          value: 'gen'
+        }
+      ]
+    }
+  ],
   executeI(client, interaction) {
     const cpu_emoji = client.emojis.cache.get("864314312028979250");
     const pc_emoji = client.emojis.cache.get("864314369856634901");
@@ -23,11 +44,14 @@ module.exports = {
     for(let i of dependencies) {
       dep += `${i[0]}: ${i[1]}\n\u3000`;
     }
-    const embed = new MessageEmbed().setColor('#ffffba')
-      .addField('General', `**❯ ${nameless_emoji}** ${client.user.tag} (${client.user.id})\n**❯ ${pc_emoji}** ${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}hr ${Math.floor(client.uptime / 60000) % 60}min ${Math.floor(client.uptime / 1000) % 60}sec\n\u200b`)
-      .addField('System', `**❯ ${ping_emoji}:** ${Math.round(client.ws.ping)}ms ws ping\n**❯ ${hdd_emoji}:** ${os.platform()},${os.release()}\n**❯ ${cpu_emoji}:**\n\u3000 ${core.model}\n\u3000 ${os.cpus().length} x ${core.speed}MHz\n**❯ ${ram_emoji}:** ${Math.floor(((os.freemem()) / 10000000)) / 100}GiB/${Math.floor(((os.totalmem()) / 10000000)) / 100}GiB\n**❯ Dependencies:**\n\u3000 Node.js: ${process.version}\n\u3000${dep}`)
-      .addField('Current Apis', api)
-      .setTimestamp();
+    var embed = new MessageEmbed().setColor('#ffffba').setTimestamp();
+    if (interaction.options.getString('option') == 'apis') {
+      embed.setDescription(`**Current Apis**\n\n${api}`)
+    } else if (interaction.options.getString('option') == 'system') {
+      embed.setDescription(`**System**\n\n**❯ ${ping_emoji}:** ${Math.round(client.ws.ping)}ms ws ping\n**❯ ${hdd_emoji}:** ${os.platform()},${os.release()}\n**❯ ${cpu_emoji}:**\n\u3000 ${core.model}\n\u3000 ${os.cpus().length} x ${core.speed}MHz\n**❯ ${ram_emoji}:** ${Math.floor(((os.freemem()) / 10000000)) / 100}GiB/${Math.floor(((os.totalmem()) / 10000000)) / 100}GiB\n**❯ Dependencies:**\n\u3000Node.js: ${process.version}\n\u3000${dep}`)
+    } else {
+      embed.setDescription(`**General**\n\n**❯ ${nameless_emoji}** ${client.user.tag} (${client.user.id})\n**❯ ${pc_emoji}** ${Math.floor(client.uptime / 86400000)}d ${Math.floor(client.uptime / 3600000) % 24}hr ${Math.floor(client.uptime / 60000) % 60}min ${Math.floor(client.uptime / 1000) % 60}sec\n\u200b`)
+    }
     interaction.reply({ embeds: [embed] });
   }
 };
