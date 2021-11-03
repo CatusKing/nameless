00028fa1586f5ffe18723c024194c140c44e6821
Thomas <chalkking17@gmail.com>
2021-11-03 08:36:20 -0400

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -15,7 +15,7 @@ module.exports = {
   ],
   executeI(client, interaction = new CommandInteraction(), log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     post({json: true, url: 'https://v1.api.amethysts.moe/generate/vs', headers: {Authorization: apiKey7}, body: {url: interaction.user.avatarURL(), avatar: interaction.options.getUser('opponent').avatarURL}}, (err, res, body) => {
-      interaction.reply(body.toString())
+      console.log(body)
     })
   }
 };
