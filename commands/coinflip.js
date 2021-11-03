4b7352b41a1604c63ee98aff1b94ce7e1f40051f
Thomas <chalkking17@gmail.com>
2021-11-03 14:31:31 -0400

    Update coinflip.js

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -14,8 +14,8 @@ module.exports = {
     }
   ],
   executeI(client, interaction = new CommandInteraction(), log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
-    post({url: 'https://v1.api.amethyste.moe/generate/vs', headers: {Authorization: 'Bearer '+apiKey7}, body: {url: interaction.user.avatarURL({format: 'png'}), avatar: interaction.options.getUser('opponent').avatarURL({format: 'png'})}}, (err, res, body) => {
-      interaction.reply({embeds: [new MessageEmbed().setImage('attachment://vs.png')], files:[new MessageAttachment(JSON.parse(body), 'vs.png')]})
+    post({url: 'https://v1.api.amethyste.moe/generate/vs', headers: {'content-type': "application/x-www-form-urlencoded", Authorization: 'Bearer '+apiKey7}, body: {url: interaction.user.avatarURL({format: 'png'}), avatar: interaction.options.getUser('opponent').avatarURL({format: 'png'})}}, (err, res, body) => {
+      interaction.reply({embeds: [new MessageEmbed().setImage('attachment://vs.png')], files:[new MessageAttachment(body, 'vs.png')]})
     })
   }
 };
