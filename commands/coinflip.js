4b14aca273330a614c25c0d1481d9df09b091a33
Thomas <chalkking17@gmail.com>
2021-11-03 08:32:51 -0400

    Create coinflip.js

---
+++ commands/coinflip.js
@@ -0,0 +1,20 @@
+const { CommandInteraction } = require('discord.js');
+const { post } = require('request');
+module.exports = {
+	name: 'coinflip',
+	description: 'Verse someone else in a coin flip',
+  slash: true,
+  options: [
+    {
+      name: 'opponent',
+      type: 'USER',
+      description: 'The person you want to verse',
+      required: true
+    }
+  ],
+  executeI(client, interaction = new CommandInteraction(), log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    post({json: true, url: 'https://v1.api.amethysts.moe/generate/vs', headers: {Authorization: token.apiKey7}, body: {url: interaction.user.avatarURL(), avatar: interaction.options.getUser('opponent').avatarURL}}, (err, res, body) => {
+      interaction.reply(body.toString())
+    })
+  }
+};
