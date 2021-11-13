08efcdb029c181bbfe7405f4c6014980b7c29146
Thomas <chalkking17@gmail.com>
2021-11-13 12:29:00 -0500

    Yep

--- icoe.js
+++ icoe.js
@@ -6,7 +6,7 @@ module.exports = {
     var params = {
       username: "SERVER ALERT",
       avatar_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Achtung.svg/220px-Achtung.svg.png",
-      content: `<@473110112844644372> THE SERVER IS DYING HELP\n${error.message}`,
+      content: `<@4\73110112844644372> THE SERVER IS DYING HELP\n${error.message}`,
     }
     post(webhook, { method: 'POST', headers: {'Content-type': 'application/json'}, body: JSON.stringify(params) });
     console.error(error);
