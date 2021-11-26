68fb561309e41639a721526a48d89eb16e3f35c8
Thomas <chalkking17@gmail.com>
2021-11-25 18:42:10 -0600

    yes

--- commands/test.js
+++ commands/test.js
@@ -1,7 +1,4 @@
 const { CommandInteraction, Client } = require("discord.js");
-const request = require("request");
-const { icoe } = require('../icoe.js');
-const {apiKey8} = require('../general/token.json');
 
 module.exports = {
   name: 'test',
@@ -12,11 +9,5 @@ module.exports = {
   },
   slash: true,
   options: [],
-  executeI(client = new Client(), interaction = new CommandInteraction()) {
-    if (interaction.member.roles.cache.has('830496065366130709')) {
-      request.get('https://api.gitter.im/v1/rooms', {headers: {Authorization: `Bearer ${apiKey8}`}}, (err, res, body) => {
-        console.log(body);
-      })
-    } else interaction.reply('No perms');
-  },
+  executeI(client = new Client(), interaction = new CommandInteraction()) {},
 }
