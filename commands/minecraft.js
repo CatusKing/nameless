36b8912c375d5eb7da3a885d1a5fc478c953c1e6
Thomas <chalkking17@gmail.com>
2021-12-11 21:25:48 -0500

    y

---
+++ commands/minecraft.js
@@ -0,0 +1,23 @@
+const { get } = require('request');
+const { icoe } = require('../icoe');
+module.exports = {
+  name: 'minecraft',
+  description: 'The command for the minecraft server',
+  slash: true,
+  options: [
+    {
+      name: 'server',
+      type: 'SUB_COMMAND',
+      description: 'Get the server status',
+      required: true,
+      options: []
+    },
+  ],
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+    if (interaction.options.getSubcommand('server')) {
+      get('https://minecraft-api.com/api/ping/mc.catusking.com/25565/json', (res, err, body) => {
+        console.log(body);
+      });
+    }
+  }
+};
