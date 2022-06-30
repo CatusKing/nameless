c4ac886f6393f860e5b29aa76a8fd5630d349112
Thomas <chalkking17@gmail.com>
2022-06-30 07:42:26 -0400

    y

--- commands/admins.js
+++ commands/admins.js
@@ -6,7 +6,7 @@ module.exports = {
   command: false,
   slash: true,
   options: [],
-  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, getUserMuted, setUserMuted, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
+  executeI(client, interaction, log, hours, getUserDaily, setUserDaily, getUserWeekly, setUserWeekly, getUserBalance, addUserBalance, floor, commands, updateLeaderboard, updateStatus, setServerAdmins, admins, setServerIgnoredCh, ignoredCh, setUserBanned, round, db) {
     var description = 'Admins\n';
     for (let i of admins) {
       description += `${client.users.cache.get(i).tag} - ${i}\n`
